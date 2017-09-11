9b658ab2acaa3f924384745cf0f8f3c1873babcd
devon <devon@jtrsolutions.com>
2017-09-11 14:20:28 -0400

    Initial model factories

---
+++ database/factories/BusinessFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(\App\Activity::class, function(Faker $faker) {
+    return [
+        'code' => str_pad(mt_rand(0, 999), 3, '0', STR_PAD_LEFT),
+        'name' => $faker->word,
+        'description' => $faker->sentence,
+    ];
+});
+
+$factory->define(\App\Business::class, function(Faker $faker) {
+    return [
+        'type' => 'Registry',
+        'address1' => $faker->streetAddress,
+        'address2' => null,
+        'city' => $faker->city,
+        'state' => $faker->randomElement(['CA', 'OH', 'NY', 'MI', 'PA', 'FL', 'TX', 'WA']),
+        'country' => 'US',
+        'zip' => $faker->randomNumber(5),
+        'phone1' => $faker->phoneNumber,
+        'phone2' => $faker->phoneNumber,
+        'default_commission_rate' => mt_rand(500, 9000) / 100,
+    ];
+});
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,13 +13,70 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
-    static $password;
-
+function userFactory(Faker $faker) {
     return [
-        'name' => $faker->name,
+        'firstname' => $faker->firstName,
+        'lastname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password = bcrypt('demo'),
         'remember_token' => str_random(10),
+        'date_of_birth' => $faker->date('Y-m-d', '-20 years'),
+    ];
+}
+
+$factory->define(\App\Admin::class, function(Faker $faker) {
+    return array_merge(userFactory($faker), []);
+});
+
+$factory->define(\App\Caregiver::class, function(Faker $faker) {
+    return array_merge(userFactory($faker), [
+        'ssn' => $faker->randomNumber(3) . '-' . $faker->randomNumber(2) . '-' . $faker->randomNumber(4),
+    ]);
+});
+
+$factory->define(\App\Client::class, function(Faker $faker) {
+    return array_merge(userFactory($faker), []);
+});
+
+$factory->define(\App\OfficeUser::class, function(Faker $faker) {
+    return array_merge(userFactory($faker), []);
+});
+
+$factory->define(\App\Address::class, function(Faker $faker) {
+    return [
+        'type' => $faker->randomElement(['billing', 'mailing']),
+        'address1' => $faker->streetAddress,
+        'address2' => null,
+        'city' => $faker->city,
+        'state' => $faker->randomElement(['CA', 'OH', 'NY', 'MI', 'PA', 'FL', 'TX', 'WA']),
+        'country' => 'US',
+        'zip' => $faker->randomNumber(5)
+    ];
+});
+
+$factory->define(\App\PhoneNumber::class, function(Faker $faker) {
+    return [
+        'type' => $faker->randomElement(['home', 'mobile', 'work']),
+        'number' => $faker->phoneNumber
+    ];
+});
+
+$factory->define(\App\CreditCard::class, function(Faker $faker) {
+    return [
+        'nickname' => $faker->streetName,
+        'name_on_card' => $faker->name,
+        'number' => $faker->creditCardNumber,
+        'expiration_month' => mt_rand(1,12),
+        'expiration_year' => mt_rand(date('Y'), date('Y') + 3),
+    ];
+});
+
+$factory->define(\App\BankAccount::class, function(Faker $faker) {
+    return [
+        'nickname' => $faker->streetName,
+        'routing_number' => $faker->bankAccountNumber,
+        'account_number' => $faker->bankAccountNumber,
+        'account_type' => 'Checking',
+        'verified' => $faker->randomElement([0,1,1])
     ];
 });
