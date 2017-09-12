dab639c63d76ab1284e34e2a046fd000f3999854
devon <devon@jtrsolutions.com>
2017-09-12 14:11:46 -0400

    Business bank account methods

--- app/Business.php
+++ app/Business.php
@@ -2,11 +2,39 @@
 
 namespace App;
 
+use App\Exceptions\ExistingBankAccountException;
 use Illuminate\Database\Eloquent\Model;
 
 class Business extends Model
 {
     protected $table = 'businesses';
+    protected $guarded = ['id'];
+
+    public function setBankAccount(BankAccount $account)
+    {
+        if ($account->id) {
+            throw new ExistingBankAccountException('setBankAccount only accepts new bank accounts.');
+        }
+
+        $account->user_id = null;
+        if (!$account->save()) {
+            throw new \Exception('Could not save the bank account to the database.');
+        }
+
+        $existingAccount = $this->bankAccount;
+        $update = $this->update(['bank_account_id' => $account->id]);
+        $this->load('bankAccount'); // reload bankAccount related model
+        if ($update && $existingAccount) {
+            $existingAccount->delete();
+        }
+
+        return $update;
+    }
+
+    public function bankAccount()
+    {
+        return $this->belongsTo(BankAccount::class);
+    }
 
     public function clients()
     {
