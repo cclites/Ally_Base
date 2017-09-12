05b2faf34291488e25e05e22fe5c2fc3732c8ebb
devon <devon@jtrsolutions.com>
2017-09-12 15:06:21 -0400

    ScheduleFactory fix: missing strtotime

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -14,14 +14,16 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(\App\Schedule::class, function(Faker $faker) {
+    $start = $faker->date();
+    $end = date('Y-m-d', strtotime($start . ' +' . (string) mt_rand(10, 1000) . ' days'));
     $rruleFreq = $faker->randomElement(['WEEKLY', 'WEEKLY', 'MONTHLY']);
     $rruleByday = $faker->randomElement(['MO', 'TU', 'WE', 'TH', 'FR', 'MO,WE', 'TU,TH', 'WE,FR', 'MO,TU', 'MO,TU,WE']);
     $rruleInterval = $faker->randomElement([1,1,1,2]);
 
     return [
-        'start_date' => $start = $faker->date(),
-        'end_date' => date('Y-m-d', $start . ' +' . mt_rand(10, 1000) . ' days'),
-        'time' => $faker->randomElement(['08', '09', '11', '14', '15']) . ':' . $faker->randomElement(['00', '15', '30']),
+        'start_date' => $start,
+        'end_date' => $end,
+        'time' => $faker->randomElement(['08', '09', '11', '14', '15']) . ':' . $faker->randomElement(['00', '15', '30']) . ':00',
         'duration' => $faker->randomElement([90, 120, 180, 240]),
         'rrule' => sprintf('FREQ=%s;BYDAY=%s;INTERVAL=%s', $rruleFreq, $rruleByday, $rruleInterval),
         'notes' => $faker->randomElement([$faker->sentence, $faker->paragraph, null])
