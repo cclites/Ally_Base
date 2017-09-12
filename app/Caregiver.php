33c243707485eb2b73184e579ea92dd95d0386b5
devon <devon@jtrsolutions.com>
2017-09-12 14:12:33 -0400

    Caregiver default bank account management

--- app/Caregiver.php
+++ app/Caregiver.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Exceptions\ExistingBankAccountException;
 use App\Traits\IsUserRole;
 use Crypt;
 use Illuminate\Database\Eloquent\Model;
@@ -14,9 +15,24 @@ class Caregiver extends Model
     public $timestamps = false;
     public $fillable = ['ssn', 'bank_account_id'];
 
+    public function setBankAccount(BankAccount $account)
+    {
+        if ($account->id && $account->user_id != $this->id) {
+            throw new ExistingBankAccountException('Bank account is owned by another user.');
+        }
+
+        if (!$account->id) {
+            if (!$this->bankAccounts()->save($account)) {
+                throw new \Exception('Unable to save bank account to database.');
+            }
+        }
+
+        return $this->update(['bank_account_id' => $account->id]);
+    }
+
     public function bankAccount()
     {
-        return $this->bankAccounts->where('bank_account_id', $this->bank_account_id)->first();
+        return $this->belongsTo(BankAccount::class);
     }
 
     public function deposits()
