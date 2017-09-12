bf261e8b4dc3f33cda2ff71b587de748c71d811b
devon <devon@jtrsolutions.com>
2017-09-12 10:45:26 -0400

    EncryptedDataTest

---
+++ tests/Feature/EncryptedDataTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\BankAccount;
+use App\Caregiver;
+use App\Client;
+use App\CreditCard;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EncryptedDataTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testSsnEncryption()
+    {
+        $ssn       = '123-45-6789';
+        $caregiver = factory(Caregiver::class)->create(['ssn' => $ssn]);
+        $encrypted = $caregiver->getAttributes()['ssn'];
+        $decrypted = $caregiver->ssn;
+        $this->assertNotEquals($encrypted, $decrypted);
+        $this->assertGreaterThan(127, strlen($encrypted));
+        $this->assertEquals($decrypted, \Crypt::decrypt($encrypted));
+        $this->assertEquals($decrypted, $ssn);
+    }
+
+    public function testCreditCardEncryption()
+    {
+        $ccNumber = '1234567812345678';
+        $client = factory(Client::class)->create();
+        $cc = factory(CreditCard::class)->make(['number' => $ccNumber]);
+        $client->user->creditCards()->save($cc);
+
+        $decrypted = $cc->number;
+        $encrypted = $cc->getAttributes()['number'];
+
+        $this->assertNotEquals($encrypted, $decrypted);
+        $this->assertGreaterThan(127, strlen($encrypted));
+        $this->assertEquals($decrypted, \Crypt::decrypt($encrypted));
+        $this->assertEquals($decrypted, $ccNumber);
+    }
+
+    public function testBankAccountEncryption()
+    {
+        $accountNo = '123456789';
+        $client = factory(Client::class)->create();
+        $account = factory(BankAccount::class)->make(['account_number' => $accountNo]);
+        $client->user->bankAccounts()->save($account);
+
+        $decrypted = $account->account_number;
+        $encrypted = $account->getAttributes()['account_number'];
+
+        $this->assertNotEquals($encrypted, $decrypted);
+        $this->assertGreaterThan(127, strlen($encrypted));
+        $this->assertEquals($decrypted, \Crypt::decrypt($encrypted));
+        $this->assertEquals($decrypted, $accountNo);
+    }
+}
