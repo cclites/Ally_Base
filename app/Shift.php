e4afd6256eee8ac2b19cb9677d9db9b7e6a06b0a
devon <devon@jtrsolutions.com>
2017-09-15 10:12:02 -0400

    Add isVerified and duration methods on shift model

--- app/Shift.php
+++ app/Shift.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Shift extends Model
@@ -31,5 +32,20 @@ class Shift extends Model
     public function issues()
     {
         return $this->hasMany(ShiftIssue::class);
+    }
+
+    public function duration()
+    {
+        if (!$this->checked_out_time) return false;
+
+        $date1 = new Carbon($this->checked_in_time);
+        $date2 = new Carbon($this->checked_out_time);
+
+        return round($date1->diffInMinutes($date2) / 60, 2);
+    }
+
+    public function isVerified()
+    {
+        return (bool) $this->verified;
     }
 }
