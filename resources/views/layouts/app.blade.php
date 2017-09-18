15d3e64fe5ac78d921a90b40cf8846a302e137ce
devon <devon@jtrsolutions.com>
2017-09-18 13:06:03 -0400

    Replace money / email icons with usd / envelope

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -303,8 +303,8 @@
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li><a href="#"><i class="fa fa-user"></i> My Profile</a></li>
-                                <li><a href="#"><i class="fa fa-money"></i> My Balance</a></li>
-                                <li><a href="#"><i class="fa fa-email"></i> Inbox</a></li>
+                                <li><a href="#"><i class="fa fa-usd"></i> My Balance</a></li>
+                                <li><a href="#"><i class="fa fa-envelope"></i> Inbox</a></li>
                                 <li role="separator" class="divider"></li>
                                 <li><a href="#"><i class="fa fa-gears"></i> Account Setting</a></li>
                                 <li role="separator" class="divider"></li>
@@ -338,7 +338,7 @@
                 <div class="profile-img"> <img src="/demo/assets/images/users/profile.png" alt="user" /> </div>
                 <!-- User profile text-->
                 <div class="profile-text"> <a href="#" class="dropdown-toggle u-dropdown" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">Markarn Doe</a>
-                    <div class="dropdown-menu animated flipInY"> <a href="#" class="dropdown-item"><i class="fa fa-user"></i> My Profile</a> <a href="#" class="dropdown-item"><i class="fa fa-money"></i> My Balance</a> <a href="#" class="dropdown-item"><i class="fa fa-email"></i> Inbox</a>
+                    <div class="dropdown-menu animated flipInY"> <a href="#" class="dropdown-item"><i class="fa fa-user"></i> My Profile</a> <a href="#" class="dropdown-item"><i class="fa fa-usd"></i> My Balance</a> <a href="#" class="dropdown-item"><i class="fa fa-envelope"></i> Inbox</a>
                         <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"><i class="fa fa-gears"></i> Account Setting</a>
                         <div class="dropdown-divider"></div> <a href="login.html" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a> </div>
                 </div>
