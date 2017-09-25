81b3c471937836a6d57a1e4760c7c1432824e111
devon <devon@jtrsolutions.com>
2017-09-25 19:28:55 -0400

    Add database seeder with demo users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $business = factory(\App\Business::class)->create([
+            'name' => 'Ally Demo Business'
+        ]);
+
+        factory(\App\Client::class)->create([
+            'firstname' => 'Demo',
+            'lastname' => 'Client',
+            'email' => 'client@allyms.com',
+            'password' => bcrypt('demo'),
+            'business_id' => $business->id
+        ]);
+
+        $officeUser = factory(\App\OfficeUser::class)->create([
+            'firstname' => 'Demo',
+            'lastname' => 'User',
+            'email' => 'officeuser@allyms.com',
+            'password' => bcrypt('demo')
+        ]);
+        $business->users()->attach($officeUser);
+
+        $caregiver = factory(\App\Caregiver::class)->create([
+            'firstname' => 'Demo',
+            'lastname' => 'Caregiver',
+            'email' => 'caregiver@allyms.com',
+            'password' => bcrypt('demo')
+        ]);
+        $business->caregivers()->attach($caregiver);
+
+        // Create Others
+        factory(\App\Business::class, 3)->create();
+        factory(\App\Client::class, 20)->create();
+        factory(\App\Caregiver::class, 10)->create();
+        factory(\App\OfficeUser::class, 6)->create();
     }
 }
