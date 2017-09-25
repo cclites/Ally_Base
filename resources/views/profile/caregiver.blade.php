ecbb6595edbf7f426e40bf6f4d63e50dc1a068e3
devon <devon@jtrsolutions.com>
2017-09-25 19:36:39 -0400

    Add profile views for other user types

---
+++ resources/views/profile/caregiver.blade.php
@@ -0,0 +1,1 @@
+@extends('profile.office_user')
---
+++ resources/views/profile/office_user.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('title', 'Profile')
+
+@section('breadcrumbs')
+    <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+    <li class="breadcrumb-item active">Profile</li>
+@endsection
+
+@section('content')
+    <!-- Nav tabs -->
+    <ul class="nav nav-pills with-padding-bottom" role="tablist">
+        <li class="nav-item">
+            <a class="nav-link active" data-toggle="tab" href="#profile" role="tab">Profile</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#addresses" role="tab">Addresses</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#phones" role="tab">Phone Numbers</a>
+        </li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+        <div class="tab-pane active" id="profile" role="tabpanel">
+            <div class="row">
+                <div class="col-lg-12">
+                    <client-profile :user="{{ $user }}" :client="{{ $user->role }}"></client-profile>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <change-password></change-password>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane" id="addresses" role="tabpanel">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <user-address title="Home Address" type="home" :address="{{ $user->addresses->where('type', 'home')->first() ?? '{}' }}"></user-address>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane" id="phones" role="tabpanel">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12">
+                    <phone-number title="Mobile Number" type="mobile" :phone="{{ json_phone($user, 'mobile') }}"></phone-number>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <phone-number title="Work Number" type="work" :phone="{{ json_phone($user, 'work') }}"></phone-number>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <phone-number title="Home Number" type="home" :phone="{{ json_phone($user, 'home') }}"></phone-number>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
