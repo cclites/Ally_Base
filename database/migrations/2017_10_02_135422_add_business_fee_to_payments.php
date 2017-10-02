b32a7706901f96a0dce3e4f3c1a88a910d2fe076
devon <devon@jtrsolutions.com>
2017-10-02 10:35:06 -0400

    Seed shifts, payments, and deposits

---
+++ database/migrations/2017_10_02_135422_add_business_fee_to_payments.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBusinessFeeToPayments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->boolean('deposited')->default(0);
+            $table->decimal('business_allotment', 9, 2);
+            $table->decimal('caregiver_allotment', 9, 2);
+            $table->decimal('system_allotment', 9, 2);
+        });
+
+        Schema::table('payment_queue', function (Blueprint $table) {
+            $table->decimal('business_allotment', 9, 2);
+            $table->decimal('caregiver_allotment', 9, 2);
+            $table->decimal('system_allotment', 9, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
