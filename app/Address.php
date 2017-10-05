db6cfa27338b35ce27f9d60e0d2918328ae442de
Devon <devon@jtrsolutions.com>
2017-10-05 14:40:28 -0400

    Store geocode and add geocode methods to Address model

--- app/Address.php
+++ app/Address.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use App\GMaps\Geocode;
+use App\GMaps\GeocodeCoordinates;
+use App\GMaps\NoGeocodeFoundException;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
@@ -12,5 +15,32 @@ class Address extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return \App\GMaps\GeocodeCoordinates|false
+     */
+    public function getGeocode($forceUpdate = false)
+    {
+        if ($this->forceUpdate || (!$this->latitude && !$this->longitude)) {
+            $fullAddress = $this->address1 . ' ' . $this->city . ', ' . $this->state . ' ' . $this->country . ' ' . $this->zip;
+            if (!$geocode = Geocode::getCoordinates($fullAddress)) {
+                return false;
+            }
+            $this->update([
+                'latitude' => $geocode->latitude,
+                'longitude' => $geocode->longitude,
+            ]);
+        }
+        else {
+            $geocode = new GeocodeCoordinates($this->latitude, $this->longitude);
+        }
+        return $geocode;
+    }
+
+    public function distanceTo($latitude, $longitude, $units = 'm')
+    {
+        $geocode = $this->getGeocode();
+        return $geocode->distanceTo($latitude, $longitude, $units);
     }
 }
