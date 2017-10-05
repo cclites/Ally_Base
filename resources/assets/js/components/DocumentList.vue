db30230974fc97b2c0fd2d77ba542a088b163d72
Kevin Mitchell <programmerkev@gmail.com>
2017-10-05 11:46:21 -0400

    Wrap document-list in b-card component

--- resources/assets/js/components/DocumentList.vue
+++ resources/assets/js/components/DocumentList.vue
@@ -1,12 +1,17 @@
 <template>
-    <table class="table">
-        <thead><tr><th>File</th></tr></thead>
-        <tbody>
-            <tr v-for="document in documents">
-                <td>{{ document.original_filename }}</td>
-            </tr>
-        </tbody>
-    </table>
+    <b-card header="Documents"
+        header-bg-variant="info"
+        header-text-variant="white"
+        >
+        <table class="table">
+            <thead><tr><th>File</th></tr></thead>
+            <tbody>
+                <tr v-for="document in documents">
+                    <td>{{ document.original_filename }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </b-card>
 </template>
 <script>
     export default {
