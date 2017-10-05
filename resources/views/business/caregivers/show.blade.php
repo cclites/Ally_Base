f5b78704ed47c6204fbebb4d2623e50bbd872a02
Devon <devon@jtrsolutions.com>
2017-10-05 12:10:43 -0400

    Show caregiver name instead of Edit Caregiver

--- resources/views/business/caregivers/show.blade.php
+++ resources/views/business/caregivers/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Caregiver')
+@section('title', $caregiver->name())
 
 @section('breadcrumbs')
     <li class="breadcrumb-item"><a href="/">Home</a></li>
