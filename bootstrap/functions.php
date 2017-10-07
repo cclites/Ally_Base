8a1def830745cabba0f8ce945a506ee52ccca98c
Devon <devon@jtrsolutions.com>
2017-10-07 11:44:34 -0400

    Filter date and time helper functions that accept offsets

--- bootstrap/functions.php
+++ bootstrap/functions.php
@@ -16,6 +16,19 @@ function filter_date($input, $to_format='Y-m-d') {
     return $carbon->format($to_format);
 }
 
+function filter_date_and_time($input_date, $input_time, $input_utc_offset, $output_date_format='Y-m-d', $output_time_format='H:i:s', $output_utc_offset='+00:00') {
+    $datetime = new DateTime($input_date . ' ' . $input_time, new DateTimeZone($input_utc_offset));
+    $datetime->setTimezone(new DateTimeZone($output_utc_offset));
+    $date = $datetime->format($output_date_format);
+    $time = $datetime->format($output_time_format);
+    return [$date, $time];
+}
+
+function filter_datetime($input_datetime, $input_utc_offset, $output_format='Y-m-d H:i:s', $output_utc_offset='+00:00') {
+    $date_and_time = filter_date_and_time($input_datetime, '', $input_utc_offset, $output_format, 'H:i:s', $output_utc_offset);
+    return $date_and_time[0];
+}
+
 function todays_date($format = 'Y-m-d') {
     return local_date('now', $format);
 }
