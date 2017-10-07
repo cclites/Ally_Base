7a1f44e7b9c25a27f39d55f28c3a8ca2d2dd97e4
Devon <devon@jtrsolutions.com>
2017-10-07 16:57:38 -0400

    Hide notifications section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -156,31 +156,31 @@
                     <!-- ============================================================== -->
                     <!-- Notifications -->
                     <!-- ============================================================== -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-muted text-muted" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-message"></i>
-                            <div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right mailbox scale-up">
-                            <ul>
-                                <li>
-                                    <div class="drop-title">Notifications</div>
-                                </li>
-                                <li>
-                                    <div class="message-center">
-                                        <!-- Message -->
-                                        <a href="#">
-                                            <div class="btn btn-danger btn-circle"><i class="fa fa-link"></i></div>
-                                            <div class="mail-contnet">
-                                                <h5>Demo Notification</h5> <span class="mail-desc">Sample notification here</span> <span class="time">9:30 AM</span> </div>
-                                        </a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
+                    {{--<li class="nav-item dropdown">--}}
+                        {{--<a class="nav-link dropdown-toggle text-muted text-muted" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="mdi mdi-message"></i>--}}
+                            {{--<div class="notify"> <span class="heartbit"></span> <span class="point"></span> </div>--}}
+                        {{--</a>--}}
+                        {{--<div class="dropdown-menu dropdown-menu-right mailbox scale-up">--}}
+                            {{--<ul>--}}
+                                {{--<li>--}}
+                                    {{--<div class="drop-title">Notifications</div>--}}
+                                {{--</li>--}}
+                                {{--<li>--}}
+                                    {{--<div class="message-center">--}}
+                                        {{--<!-- Message -->--}}
+                                        {{--<a href="#">--}}
+                                            {{--<div class="btn btn-danger btn-circle"><i class="fa fa-link"></i></div>--}}
+                                            {{--<div class="mail-contnet">--}}
+                                                {{--<h5>Demo Notification</h5> <span class="mail-desc">Sample notification here</span> <span class="time">9:30 AM</span> </div>--}}
+                                        {{--</a>--}}
+                                    {{--</div>--}}
+                                {{--</li>--}}
+                                {{--<li>--}}
+                                    {{--<a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>--}}
+                                {{--</li>--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
+                    {{--</li>--}}
                     <!-- ============================================================== -->
                     <!-- End Notifications -->
                     <!-- ============================================================== -->
