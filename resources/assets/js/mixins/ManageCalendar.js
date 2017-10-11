fa0f1261a7f43c628cd9483857d5048f1230d9d1
Devon <devon@jtrsolutions.com>
2017-10-11 12:08:12 -0400

    Fix time when clicking "Create a Schedule" button * Rounds up to the next hour mark

--- resources/assets/js/mixins/ManageCalendar.js
+++ resources/assets/js/mixins/ManageCalendar.js
@@ -16,7 +16,7 @@ export default {
                 this.selectedEvent = date;
             }
             else {
-                this.selectedEvent = moment().local();
+                this.selectedEvent = moment().local().add(59, 'minutes').startOf('hour');
             }
         },
 
