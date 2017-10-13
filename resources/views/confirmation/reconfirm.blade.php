922dc6c29d9645977ca402348daa6484a340e753
Devon <devon@jtrsolutions.com>
2017-10-13 16:27:45 -0400

    Change title verbiage

--- resources/views/confirmation/reconfirm.blade.php
+++ resources/views/confirmation/reconfirm.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.guest')
 
-@section('title', 'Confirmation')
+@section('title', 'Please confirm your information below.')
+
+@section('breadcrumbs', 'Then, click the "Accept and Verify" button at the bottom of the page.')
 
 @section('content')
     <client-confirmation id="{{ $encrypted_id }}" :client="{{ $client }}" :user="{{ $client->user }}" phone-number="{{ $phoneNumber }}" :address="{{ $client->evvAddress OR '{}' }}"></client-confirmation>
