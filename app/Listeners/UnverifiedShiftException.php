7e5fe7a5e466eab46e3b0d4930eaa385b0e3d18d
Devon <devon@jtrsolutions.com>
2017-10-27 12:12:25 -0400

    Update verbiage on Unverified Shift Exception

--- app/Listeners/UnverifiedShiftException.php
+++ app/Listeners/UnverifiedShiftException.php
@@ -27,11 +27,11 @@ class UnverifiedShiftException
     {
         $shift = $event->shift;
         $business = $shift->business;
-        $checked_in_time = local_date($shift->checked_in_time, 'm/d/Y h:iA', $business->timezone);
-        $checked_out_time = local_date($shift->checked_out_time, 'm/d/Y h:iA', $business->timezone);
+        $checked_in_time = local_date($shift->checked_in_time, 'm/d/Y g:iA', $business->timezone);
+        $checked_out_time = local_date($shift->checked_out_time, 'm/d/Y g:iA', $business->timezone);
         $description = $shift->caregiver->name() . "'s shift for " . $shift->client->name() .
-            "clocked in at $checked_in_time and clocked out at $checked_out_time was NOT verified through our geolocation or telefony services. " .
-            "You will need to manually approve this shift to qualify it for payment.";
+            " clocked in at $checked_in_time and clocked out at $checked_out_time was NOT verified through our geolocation or telefony services." .
+            " You will need to manually approve this shift to qualify it for payment.";
 
         $exception = new SystemException([
             'title' => 'Unverified Shift for ' . $shift->caregiver->name(),
