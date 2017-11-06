ce8b965931ed84e2448a32d6367038979291f3a8
Devon <devon@jtrsolutions.com>
2017-11-06 15:40:15 -0500

    Abstract base import methods

--- app/Console/Commands/BaseImport.php
+++ app/Console/Commands/BaseImport.php
@@ -7,22 +7,27 @@ use App\Business;
 use App\Caregiver;
 use App\PhoneNumber;
 use App\User;
+use Illuminate\Console\Command;
+use PHPExcel_IOFactory;
+use PHPExcel_Shared_Date;
 
-class ImportGenerationsCaregivers extends BaseImport
+abstract class BaseImport extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'import:generations_caregivers {business_id} {file}';
 
     /**
-     * The console command description.
-     *
-     * @var string
+     * @var \PHPExcel
      */
-    protected $description = 'Import a "Generations" excel export of caregivers into the system.';
+    protected $sheet;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     /**
      * Execute the console command.
@@ -36,8 +41,11 @@ class ImportGenerationsCaregivers extends BaseImport
         sleep(2);
 
         $objPHPExcel = $this->loadSheet();
-
-        $lastRow = $this->getRowCount($objPHPExcel);
+        $lastRow = (int) $objPHPExcel->setActiveSheetIndex(0)->getHighestRow();
+        if (!$lastRow) {
+            $this->output->error('Error getting row count.  Is this spreadsheet empty?');
+            exit;
+        }
 
         for($row=2; $row<$lastRow; $row++) {
 
@@ -70,8 +78,8 @@ class ImportGenerationsCaregivers extends BaseImport
 
                 // Create caregiver record
                 // Fake username and email
-                $data['username'] = 'placeholder' . uniqid();
-                $data['email'] = 'placeholder' . uniqid();
+                $data['username'] = 'placeholder' . time();
+                $data['email'] = 'placeholder' . time();
                 $caregiver = new Caregiver($data);
                 if ($email) {
                     $caregiver->email = $email;
@@ -113,5 +121,63 @@ class ImportGenerationsCaregivers extends BaseImport
 
         }
 
+    }
+
+    /**
+     * @return \PHPExcel
+     */
+    public function loadSheet()
+    {
+        if (!$objPHPExcel = PHPExcel_IOFactory::load($this->argument('file'))) {
+            $this->output->error('Could not load file: ' . $this->argument('file'));
+            exit;
+        }
+        return $this->sheet = $objPHPExcel;
+    }
+
+    public function getRowCount(\PHPExcel $PHPExcel)
+    {
+        $lastRow = (int) $PHPExcel->setActiveSheetIndex(0)->getHighestRow();
+        if (!$lastRow) {
+            $this->output->error('Error getting row count.  Is this spreadsheet empty?');
+            exit;
+        }
+        return $lastRow;
+    }
+
+    public function getValue(\PHPExcel $PHPExcel, $header, $rowNo)
+    {
+        $column = $this->findColumn($PHPExcel, $header);
+        if ($column === false) {
+            return null;
+        }
+        $cell = $PHPExcel->getActiveSheet()->getCell($column . $rowNo);
+        $value = $cell->getValue();
+
+        if(PHPExcel_Shared_Date::isDateTime($cell)) {
+            return date('Y-m-d', PHPExcel_Shared_Date::ExcelToPHP($value));
+        }
+
+        return $value;
+    }
+
+    public function findColumn(\PHPExcel $PHPExcel, $header)
+    {
+        // Get range from A to BZ
+        $a_z = range('A', 'Z');
+        $range = array_merge(
+            $a_z,
+            array_map(function($val) { return 'A' . $val; }, $a_z),
+            array_map(function($val) { return 'B' . $val; }, $a_z)
+        );
+
+        foreach($range as $column) {
+            $value = $PHPExcel->getActiveSheet()->getCell($column . '1')->getValue();
+            if ($value == $header) {
+                return $column;
+            }
+        }
+
+        return false;
     }
 }
