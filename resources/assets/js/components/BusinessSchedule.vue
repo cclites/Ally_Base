b6686329a213c3a4ba6f8d86f56cf2b57fc19e23
Mike <mike.shinobi@gmail.com>
2017-11-07 17:18:42 -0600

    Fixed css class

--- resources/assets/js/components/BusinessSchedule.vue
+++ resources/assets/js/components/BusinessSchedule.vue
@@ -1,7 +1,7 @@
 <template>
     <b-card>
         <b-btn size="sm" variant="info" @click="createSchedule()"><i class="fa fa-plus"></i> Create a Schedule</b-btn>
-        <b-btn size="lg" variant="info" class="pull-right"><i class="fa-clock-o"></i> Clock In</b-btn>
+        <b-btn size="lg" variant="info" class="pull-right"><i class="fa fa-clock-o"></i> Clock In</b-btn>
         <full-calendar ref="calendar" :events="events" default-view="agendaWeek" :header="header" @day-click="createSchedule" @event-selected="editSchedule"  />
 
         <create-schedule-modal :model.sync="createModal"
