7cf09f98466ea2216485acde872d3ee64d87de01
Devon <devon@jtrsolutions.com>
2017-11-22 09:55:23 -0500

    Include address and phone number in transactions, code to interfaces

--- app/CreditCard.php
+++ app/CreditCard.php
@@ -3,10 +3,12 @@
 
 namespace App;
 
+use App\Contracts\ChargeableInterface;
+use App\Gateway\CreditCardPaymentInterface;
 use Crypt;
 use Illuminate\Database\Eloquent\Model;
 
-class CreditCard extends Model
+class CreditCard extends Model implements ChargeableInterface
 {
     protected $table = 'credit_cards';
     protected $guarded = ['id'];
@@ -45,4 +47,20 @@ class CreditCard extends Model
     /// Other Methods
     ///////////////////////////////////////////
 
+    /**
+     * @param float $amount
+     * @param string $currency
+     * @return \App\GatewayTransaction|false
+     */
+    public function charge($amount, $currency = 'USD')
+    {
+        $gateway = app()->make(CreditCardPaymentInterface::class);
+        if ($this->user && $address = $this->user->addresses->where('type', 'billing')->first()) {
+            $gateway->setBillingAddress($address);
+        }
+        if ($this->user && $phone = $this->user->phoneNumbers->where('type', 'billing')->first()) {
+            $gateway->setBillingPhone($phone);
+        }
+        return $gateway->chargeCard($this, $amount, $currency);
+    }
 }
--- app/Gateway/CreditCardPaymentInterface.php
+++ app/Gateway/CreditCardPaymentInterface.php
@@ -2,7 +2,9 @@
 
 namespace App\Gateway;
 
+use App\Address;
 use App\CreditCard;
+use App\PhoneNumber;
 
 interface CreditCardPaymentInterface
 {
@@ -42,4 +44,16 @@ interface CreditCardPaymentInterface
      * @throws \App\Exceptions\PaymentMethodDeclined|\App\Exceptions\PaymentMethodError
      */
     public function chargeCard(CreditCard $card, $amount, $currency = 'USD', $cvv = null);
+
+    /**
+     * @param \App\Address $address
+     * @return $this
+     */
+    public function setBillingAddress(Address $address);
+
+    /**
+     * @param \App\PhoneNumber $phone
+     * @return $this
+     */
+    public function setBillingPhone(PhoneNumber $phone);
 }
