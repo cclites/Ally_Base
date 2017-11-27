7a37b6f32e7ae165999a283c0e4393bae548720e
Devon <devon@jtrsolutions.com>
2017-11-27 08:36:07 -0500

    Filters on scheduled vs actual report

--- resources/assets/js/components/BusinessScheduledVsActual.vue
+++ resources/assets/js/components/BusinessScheduledVsActual.vue
@@ -26,13 +26,33 @@
         <b-row>
             <b-col lg="12">
                 <b-card
-                        header="Missing Shifts"
+                        header="Scheduled Shifts with No Clock-in"
                         header-text-variant="white"
                         header-bg-variant="info"
                 >
+                    <b-row>
+                        <b-col sm="6">
+                        </b-col>
+                        <b-col sm="6">
+                            <b-row>
+                                <b-col cols="6">
+                                    <b-form-select v-model="filterCaregiverId">
+                                        <option value="">All Caregivers</option>
+                                        <option v-for="item in caregivers" :value="item.id">{{ item.nameLastFirst }}</option>
+                                    </b-form-select>
+                                </b-col>
+                                <b-col cols="6">
+                                    <b-form-select v-model="filterClientId">
+                                        <option value="">All Clients</option>
+                                        <option v-for="item in clients" :value="item.id">{{ item.nameLastFirst }}</option>
+                                    </b-form-select>
+                                </b-col>
+                            </b-row>
+                        </b-col>
+                    </b-row>
                     <b-table bordered striped hover show-empty
                              :fields="fields"
-                             :items="items"
+                             :items="filteredItems"
                              :sort-by.sync="sortBy"
                              :sort-desc.sync="sortDesc"
                              class="shift-table"
@@ -74,11 +94,16 @@
                 sortDesc: null,
                 selectedItem: {},
                 convertModal: false,
+                clients: [],
+                caregivers: [],
+                filterCaregiverId: "",
+                filterClientId: "",
             }
         },
 
         mounted() {
             this.loadData();
+            this.loadFiltersData();
         },
 
         computed: {
@@ -91,6 +116,8 @@
                         if (key === 'schedule_id') continue;
                         if (key === 'shift_id') continue;
                         if (key === 'converted') continue;
+                        if (key === 'caregiver_id') continue;
+                        if (key === 'client_id') continue;
                         fields.push({
                             'key': key,
                             'sortable': true,
@@ -100,6 +127,18 @@
                 fields.push('actions');
                 return fields;
             },
+
+            filteredItems() {
+                let items = this.items;
+                if (this.filterCaregiverId || this.filterClientId) {
+                    items = items.filter(item => {
+                        if (this.filterCaregiverId && this.filterCaregiverId != item.caregiver_id) return false;
+                        if (this.filterClientId && this.filterClientId != item.client_id) return false;
+                        return true;
+                    });
+                }
+                return items;
+            }
         },
 
         methods: {
@@ -113,6 +152,8 @@
                                 return {
                                     'key': counter++,
                                     'schedule_id': item.schedule_id,
+                                    'caregiver_id': item.caregiver_id,
+                                    'client_id': item.client_id,
                                     'converted': false,
                                     //
                                     'Day': item.start,
@@ -133,6 +174,11 @@
                             component.items = [];
                         }
                     });
+            },
+
+            loadFiltersData() {
+                axios.get('/business/clients').then(response => this.clients = response.data);
+                axios.get('/business/caregivers').then(response => this.caregivers = response.data);
             },
 
             dayFormat(date) {
