b207da8d9e7d615fc188f7c901660f9a153eff5b
James Sweeney <wildwestriverrider@gmail.com>
2017-12-12 13:31:04 -0700

    Client online setup report.

--- resources/views/menu/office_user.blade.php
+++ resources/views/menu/office_user.blade.php
@@ -53,6 +53,7 @@
         <li><a href="{{ route('business.reports.cc_expiration') }}">Credit Card Expiration</a></li>
         <li><a href="{{ route('business.reports.client_caregivers') }}">Client Caregiver Rates</a></li>
         <li><a href="{{ route('business.reports.client_email_missing') }}">Clients without Email</a></li>
+        <li><a href="{{ route('business.reports.client_onboarded') }}">Client Online Setup</a></li>
     </ul>
 </li>
 <li>
