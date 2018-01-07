8b5fca05ce11d178530728fe1d3152b8596d2942
Daniel Alamo <daniel.alamo@gmail.com>
2018-01-07 16:31:03 -0500

    Add Admin/ClientController

---
+++ app/Http/Controllers/Admin/ClientController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Client;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @param Request $request
+     */
+    public function index(Request $request)
+    {
+        $clients = Client::orderBy('id')->get();
+        return response($clients);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(){}
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param Request $request
+     */
+    public function store(Request $request){}
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param Client $client
+     */
+    public function show(Client $client){}
+
+    /**
+     * Show the form for editing the specified resource.
+     * 
+     * @param Client $client
+     */
+    public function edit(Client $client){}
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param Request $request
+     * @param Client $client
+     */
+    public function update(Request $request, Client $client){}
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param Client $business
+     */
+    public function destroy(Client $client){}
+}
