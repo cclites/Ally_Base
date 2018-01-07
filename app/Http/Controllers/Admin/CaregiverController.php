85ca76041bbbc00d6175daa92d653ae4536ea09f
Daniel Alamo <daniel.alamo@gmail.com>
2018-01-07 16:30:53 -0500

    Add Admin/CaregiverController

---
+++ app/Http/Controllers/Admin/CaregiverController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Caregiver;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CaregiverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @param Request $request
+     */
+    public function index(Request $request)
+    {
+        $caregivers = Caregiver::orderBy('id')->get();
+        return response($caregivers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(){}
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param Request $request
+     */
+    public function store(Request $request){}
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param Caregiver $caregiver
+     */
+    public function show(Caregiver $caregiver){}
+
+    /**
+     * Show the form for editing the specified resource.
+     * 
+     * @param Caregiver $caregiver
+     */
+    public function edit(Caregiver $caregiver){}
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param Request $request
+     * @param Caregiver $caregiver
+     */
+    public function update(Request $request, Caregiver $caregiver){}
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param Caregiver $business
+     */
+    public function destroy(Caregiver $caregiver){}
+}
