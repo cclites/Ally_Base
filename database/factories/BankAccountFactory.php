1600542cd3eb017ca2827f72713a034ae403bb61
James Sweeney <wildwestriverrider@gmail.com>
2018-01-09 14:49:33 -0700

    Test a client can add a bank account.

--- database/factories/BankAccountFactory.php
+++ database/factories/BankAccountFactory.php
@@ -2,8 +2,21 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(BankAccount::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory(\App\OfficeUser::class)->create()->user->id;
+        },
+        'business_id' => function () {
+            return factory(\App\Business::class)->create()->id;
+        },
+        'nickname' => $faker->word,
+        'name_on_account' => $faker->name,
+        'routing_number' => $this->faker->randomNumber(9),
+        'account_number' => $faker->bankAccountNumber,
+        'account_type' => collect('checking', 'savings')->random(),
+        'account_holder_type' => collect('business', 'personal')->random(),
+        'verified' => $faker->boolean
+
     ];
 });
--- tests/Controller/Client/PaymentMethodTest.php
+++ tests/Controller/Client/PaymentMethodTest.php
@@ -4,6 +4,7 @@ namespace Tests\Controller\Client;
 
 use App\Client;
 use App\CreditCard;
+use App\BankAccount;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -15,7 +16,7 @@ class PaymentMethodTest extends TestCase
     /**
      * @return void
      */
-    public function testAClientCanAddACreditCard()
+    public function testAClientCanAddAPrimaryCreditCard()
     {
         $client = factory(Client::class)->create();
 
@@ -36,5 +37,32 @@ class PaymentMethodTest extends TestCase
             ->where('name_on_card', $credit_card->name_on_card)
             ->get();
         $this->assertCount(1, $cards);
+    }
+
+    public function testAClientCanAddAPrimaryBankAccount()
+    {
+        $client = factory(Client::class)->create();
+
+        $this->actingAs($client->user);
+        $bank_account = factory(BankAccount::class)->make([
+            'user_id' => $client->user->id,
+            'business_id' => null
+        ]);
+        $data = collect($bank_account->toArray())->except('last_four', 'user_id', 'business_id')->toArray();
+        $data['routing_number'] = $this->faker->randomNumber(9);
+        $data['routing_number_confirmation'] = $data['routing_number'];
+        $data['account_number'] = $this->faker->bankAccountNumber;
+        $data['account_number_confirmation'] = $data['account_number'];
+
+        $response = $this->post('/profile/payment/primary', $data);
+
+        $response->assertStatus(200);
+
+        $accounts = BankAccount::where('user_id', $client->user->id)
+            ->where('nickname', $bank_account->nickname)
+            ->where('name_on_account', $bank_account->name_on_account)
+            ->get();
+
+        $this->assertCount(1, $accounts);
     }
 }
