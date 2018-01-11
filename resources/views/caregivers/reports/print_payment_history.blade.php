11f88466d4aa47e027293165f2319b6eda434d32
James Sweeney <wildwestriverrider@gmail.com>
2018-01-11 11:16:49 -0700

    Caregiver payment history print page test.

--- resources/views/caregivers/reports/print_payment_history.blade.php
+++ resources/views/caregivers/reports/print_payment_history.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.print')
 
+@section('title')
+    Payment History
+@endsection
+
 @section('content')
     <div class="container">
         <div class="row" style="margin-top: 1rem;">
