1b72009103d2583c40b51094d9bf5f3f8cb83605
Devon <devon@jtrsolutions.com>
2018-01-12 11:27:01 -0500

    ALLY-19 Verbiage update

--- resources/views/emails/caregiver/license_expiration_reminder.blade.php
+++ resources/views/emails/caregiver/license_expiration_reminder.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 Hello {{ $caregiver->user->firstname }},
 
-This is a friendly reminder that your {{ $license->name }} certification is set to expire on {{ $license->expires_at->format('m/d/Y') }}.
+This is a friendly reminder that, according to our records, your {{ $license->name }} certification expires on {{ $license->expires_at->format('m/d/Y') }}.
 Please contact {{ $business->name }} with your updated certification information as soon as possible.
 
 Thank you!
