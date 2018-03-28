570f21388439e37e723a785e44193f15709bf129
faze <jonwillard11@gmail.com>
2018-03-28 10:34:37 -0400

    Added timestamp methods to status manager

--- app/Shifts/ShiftStatusManager.php
+++ app/Shifts/ShiftStatusManager.php
@@ -2,6 +2,7 @@
 namespace App\Shifts;
 
 use App\Shift;
+use App\ShiftStatusHistory;
 
 class ShiftStatusManager
 {
@@ -223,6 +224,26 @@ class ShiftStatusManager
             $this->status(),
             self::getUnconfirmedStatuses()
         );
+    }
+
+     /**
+     * Returns the timestamp for when the shift was last confirmed
+     *
+     * @return void
+     */
+    public function confirmedAt()
+    {
+        return ShiftStatusHistory::getDateForStatus($this->shift, 'WAITING_FOR_AUTHORIZATION');
+    }
+
+    /**
+     * Returns the timestamp for when the shift was last charged
+     *
+     * @return void
+     */
+    public function chargedAt()
+    {
+        return ShiftStatusHistory::getDateForStatus($this->shift, 'WAITING_FOR_PAYOUT');
     }
 
     /**
