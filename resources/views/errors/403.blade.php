0653b3649a5bf1e7baacafb12f17b5da37a8e20b
Devon <devon@jtrsolutions.com>
2018-04-23 14:15:55 -0400

    ALLY-269  Add logout button to 403 and 404 error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.errors')
 
+@section('title', 'Access Forbidden')
+
+@push('head')
+    <style>
+        .btn {
+            margin-right: 15px;
+        }
+    </style>
+@endpush
+
 @section('content')
     <h1>Whoops! You don't have access to that.</h1>
-    <button type="button" onclick="window.history.back()" class="btn btn-lg btn-primary" style="margin-right: 15px">Go Back</button>
+    <button type="button" onclick="window.history.back()" class="btn btn-lg btn-primary">Go Back</button>
     <a href="/" class="btn btn-lg btn-warning">Return to Home Page</a>
+    <a href="/logout" class="btn btn-lg btn-danger">Logout</a>
 @endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.errors')
 
+@section('title', 'Page not found')
+
+@push('head')
+    <style>
+        .btn {
+            margin-right: 15px;
+        }
+    </style>
+@endpush
+
 @section('content')
     <h1>Whoops! We couldn't find the page you were looking for.</h1>
-    <button type="button" onclick="window.history.back()" class="btn btn-lg btn-primary" style="margin-right: 15px">Go Back</button>
+    <button type="button" onclick="window.history.back()" class="btn btn-lg btn-primary">Go Back</button>
     <a href="/" class="btn btn-lg btn-warning">Return to Home Page</a>
+    <a href="/logout" class="btn btn-lg btn-danger">Logout</a>
 @endsection
