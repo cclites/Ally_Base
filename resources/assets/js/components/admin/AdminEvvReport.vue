03b1e50c0f314479d73d937b511f2b128f50865f
Devon <devon@jtrsolutions.com>
2018-05-08 13:54:43 -0400

    Add client and caregiver filters to EVV Report

--- resources/assets/js/components/admin/AdminEvvReport.vue
+++ resources/assets/js/components/admin/AdminEvvReport.vue
@@ -26,6 +26,24 @@
                             <option value="">All Providers</option>
                             <option v-for="business in businesses" :value="business.id">{{ business.name }}</option>
                         </b-form-select>
+                        <b-form-select
+                                id="caregiver_id"
+                                name="caregiver_id"
+                                v-model="caregiver_id"
+                        >
+                            <option value="">--Select a Caregiver--</option>
+                            <option value="">All Caregivers</option>
+                            <option v-for="caregiver in caregivers" :value="caregiver.id">{{ caregiver.nameLastFirst }}</option>
+                        </b-form-select>
+                        <b-form-select
+                                id="client_id"
+                                name="client_id"
+                                v-model="client_id"
+                        >
+                            <option value="">--Select a Client--</option>
+                            <option value="">All Clients</option>
+                            <option v-for="client in clients" :value="client.id">{{ client.nameLastFirst }}</option>
+                        </b-form-select>
                         &nbsp;&nbsp;<b-button type="submit" variant="info">Generate Report</b-button>
                     </b-form>
                 </b-card>
@@ -68,6 +86,10 @@
                 end_date: moment().startOf('isoweek').subtract(1, 'days').format('MM/DD/YYYY'),
                 business_id: "",
                 businesses: [],
+                caregiver_id: "",
+                caregivers: [],
+                client_id: "",
+                clients: [],
                 items: [],
                 fields: [
                     {
@@ -148,7 +170,7 @@
         },
 
         mounted() {
-            this.loadBusinesses();
+            this.loadFilters();
             // this.loadItems();
         },
 
@@ -158,13 +180,15 @@
 
         methods: {
 
-            loadBusinesses() {
+            loadFilters() {
                 axios.get('/admin/businesses').then(response => this.businesses = response.data);
+                axios.get('/admin/caregivers').then(response => this.caregivers = response.data);
+                axios.get('/admin/clients').then(response => this.clients = response.data);
             },
 
             loadItems() {
-                let url = '/admin/reports/evv?json=1&start_date=' + this.start_date + '&end_date=' + this.end_date;
-                if (this.business_id) url = url + '&business_id=' + this.business_id;
+                let url = '/admin/reports/evv?json=1&start_date=' + this.start_date + '&end_date=' + this.end_date +
+                    '&business_id=' + this.business_id + '&caregiver_id=' + this.caregiver_id +  '&client_id=' + this.client_id;
                 axios.get(url)
                     .then(response => {
                         this.items = response.data.map(function (item) {
@@ -174,8 +198,8 @@
                             item.client_name = item.client.nameLastFirst;
                             item.checked_in_method = item.checked_in_number ? 'Telephony' : 'Geolocation';
                             item.checked_out_method = item.checked_out_number ? 'Telephony' : 'Geolocation';
-                            item.os = (item.user_agent.os) ? item.user_agent.os.family + ' ' + (item.user_agent.os.major ? item.user_agent.os.major : '') : '';
-                            item.browser = (item.user_agent.ua) ? item.user_agent.ua.family + ' ' + (item.user_agent.ua.major ? item.user_agent.ua.major : '') : '';
+                            item.os = (item.user_agent.os) ? item.user_agent.os.family + (item.user_agent.os.major ? ' ' + item.user_agent.os.major : '') : '';
+                            item.browser = (item.user_agent.ua) ? item.user_agent.ua.family + (item.user_agent.ua.major ? ' ' + item.user_agent.ua.major : '') : '';
                             return item;
                         });
                     });
