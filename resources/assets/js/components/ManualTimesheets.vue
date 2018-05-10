dfd9de2d90c8d1ae8f8dbda4a26469799b4c4764
faze <jonwillard11@gmail.com>
2018-05-10 15:01:43 -0400

    WIP update shift forms complete

--- resources/assets/js/components/ManualTimesheets.vue
+++ resources/assets/js/components/ManualTimesheets.vue
@@ -46,7 +46,7 @@
                             v-model="form.week"
                     >
                         <option value="">-- Select Week --</option>
-                        <option v-for="item in weekRanges" :value="item" :key="item">{{ item }}</option>
+                        <option v-for="item in weekRanges" :value="item" :key="item.id">{{ item.display }}</option>
                     </b-form-select>
                     <input-help :form="form" field="week" text=""></input-help>
                 </b-form-group>
@@ -65,13 +65,13 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="day in weekdays" :key="day">
-                        <th>{{ day }}</th>
+                    <tr v-for="shift in form.shifts" :key="shift.date">
+                        <th>{{ dow(shift.date) }}</th>
                         <td>
-                            1:00 PM - 5:32 PM
+                            {{ shiftDisplayTime(shift) }}
                         </td>
                         <td>
-                            <b-button variant="info" size="xs" @click="showModal = true">
+                            <b-button variant="info" size="xs" @click="editShift(shift)">
                                 <i class="fa fa-edit"></i>
                             </b-button>
                         </td>
@@ -87,46 +87,82 @@
             </b-col>
         </b-row>
 
-        <b-modal title="Sunday 05/25/18" v-model="showModal" size="lg">
+        <b-modal :title="modalTitle" v-model="showModal" size="lg">
             <b-container fluid>
-
-                <!-- ACTIVITIES / NOTES -->
                 <b-row>
                     <b-col md="12">
-                        <label>Activities Performed</label>
-                        <div class="form-check">
-                            <label class="custom-control custom-checkbox" v-for="activity in activities" :key="activity.id" style="clear: left; float: left;">
-                                <input type="checkbox" class="custom-control-input" v-model="form.activities" :value="activity.id">
-                                <span class="custom-control-indicator"></span>
-                                <span class="custom-control-description">{{ activity.code }} - {{ activity.name }}</span>
-                            </label>
-                        </div>
-                    </b-col>
-                </b-row>
-                <b-row class="mt-3">
-                    <b-col md="12">
+
+                        <!-- start_time -->
+                        <b-form-group label="Clocked In" label-for="start_time">
+                            <time-picker v-model="shiftForm.start_time" placeholder="HH:MM"></time-picker>
+                            <input-help :form="shiftForm" field="start_time" text=""></input-help>
+                        </b-form-group>
+
+                        <!-- end_time -->
+                        <b-form-group label="Clocked Out" label-for="end_time">
+                            <time-picker v-model="shiftForm.end_time" placeholder="HH:MM"></time-picker>
+                            <input-help :form="shiftForm" field="end_time" text=""></input-help>
+                        </b-form-group>
+
+                        <!-- activities -->
+                        <b-form-group label="Activities Performed Out" label-for="">
+                            <input-help :form="shiftForm" field="activities" text=""></input-help>
+                            <div class="form-check">
+                                <label class="custom-control custom-checkbox" v-for="activity in activities" :key="activity.id" style="clear: left; float: left;">
+                                    <input type="checkbox" class="custom-control-input" v-model="shiftForm.activities" :value="activity.id">
+                                    <span class="custom-control-indicator"></span>
+                                    <span class="custom-control-description">{{ activity.code }} - {{ activity.name }}</span>
+                                </label>
+                            </div>
+                        </b-form-group>
+
+                        <!-- mileage -->
+                        <b-form-group label="Mileage" label-for="mileage">
+                            <b-form-input
+                                    id="mileage"
+                                    name="mileage"
+                                    type="number"
+                                    v-model="shiftForm.mileage"
+                                    step="any"
+                                    min="0"
+                                    max="1000"
+                            />
+                            <input-help :form="shiftForm" field="mileage" text=""></input-help>
+                        </b-form-group>
+                        
+                        <!-- other_expenses -->
+                        <b-form-group label="Other Expenses" label-for="other_expenses">
+                            <b-form-input
+                                    id="other_expenses"
+                                    name="other_expenses"
+                                    type="number"
+                                    v-model="shiftForm.other_expenses"
+                                    step="any"
+                                    min="0"
+                                    max="1000"
+                            />
+                            <input-help :form="shiftForm" field="other_expenses" text=""></input-help>
+                        </b-form-group>
+
                         <b-form-group label="Notes" label-for="caregiver_comments">
                             <b-textarea
                                     id="caregiver_comments"
                                     name="caregiver_comments"
-                                    :rows="8"
-                                    v-model="form.caregiver_comments"
+                                    :rows="4"
+                                    v-model="shiftForm.caregiver_comments"
                             ></b-textarea>
-                            <input-help :form="form" field="caregiver_comments" text=""></input-help>
+                            <input-help :form="shiftForm" field="caregiver_comments" text=""></input-help>
                         </b-form-group>
+
                     </b-col>
                 </b-row>
-                <!-- /end ACTIVITIES / NOTES -->
-                
             </b-container>
+
             <div slot="modal-footer">
-                <b-button variant="success" type="submit" @click="showModal = false">Save</b-button>
+                <b-button variant="success" type="submit" @click="updateShift()">Save</b-button>
                 <b-btn variant="default" @click="showModal = false">Close</b-btn>
             </div>
         </b-modal>
-
-
-
     </b-card>
 </template>
 
@@ -149,21 +185,19 @@
 
         data() {
             return{
-                weekdays: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
                 showModal: false,
                 caregiver: {},
                 client: {},
-                weekRanges: ['4/29/18 - 5/5/18'],
+                weekRanges: [],
                 
                 form: new Form({
                     client_id: '',
                     caregiver_id: '',
                     shifts: [],
-                    activities: [],
-                    caregiver_comments: '',
-                    week: '',
+                    week: {},
                 }),
-                shiftForm: new Form(this.newShift()),
+
+                shiftForm: new Form({}),
             }
         },
 
@@ -172,21 +206,59 @@
                 this.caregiver = this.cg;
                 this.form.caregiver_id = this.caregiver.id;
             }
+
+            this.weekRanges = this.generateWeeks();
+            this.form.week = this.weekRanges[0];
         },
 
         methods: {
-            newShift() {
-                console.log("default rate;");
-                console.log(this.defaultRate);
-                return {
-                    date: '',
-                    start_time: '',
-                    end_time: '',
-                    mileage: 0,
-                    other_expenses: 0,
-                    caregiver_rate: this.defaultRate || 0,
-                    provider_fee: this.defaultFee || 0,
+            editShift(shift) {
+                this.shiftForm = new Form(shift);
+                this.showModal = true;
+            },
+
+            updateShift() {
+                console.log(this.shiftForm.data());
+
+                this.shiftForm.clearError();
+                if (!this.isValidShift(this.shiftForm)) {
+                    return;
                 }
+
+                var index = this.form.shifts.findIndex(x => x.date == this.shiftForm.date);
+                this.form.shifts[index] = this.shiftForm.data();
+
+                this.showModal = false;
+            },
+
+            dow(date, full = false) {
+                return moment(date).format(full ? 'dddd' : 'ddd');
+            },
+
+            shiftDisplayTime(shift) {
+                if (shift.start_time == '' || shift.end_time == '') {
+                    return '-';
+                }
+
+                return `${shift.start_time} - ${shift.end_time}`;
+            },
+
+            generateShiftsForWeek(week) {
+                let shifts = [];
+                week.days.forEach( (date) => {
+                    shifts.push({
+                        date: date,
+                        start_time: '',
+                        end_time: '',
+                        mileage: 0,
+                        other_expenses: 0,
+                        caregiver_rate: this.defaultRate || 0,
+                        provider_fee: this.defaultFee || 0,
+                        caregiver_comments: '',
+                        activities: [],
+                    });
+                });
+                return shifts;
             },
 
             submit() {
@@ -200,49 +272,34 @@
                     });
             },
 
-            addShift() {
-                this.shiftForm.clearError();
-                if (!this.isValidShift(this.shiftForm)) {
-                    return;
-                }
-
-                this.form.shifts.push(this.shiftForm.data())
-
-                this.shiftForm = new Form(this.newShift());
-            },
-
-            removeShift(index) {
-                this.form.shifts.splice(index, 1);
-            },
-
             isValidShift(data) {
-                if (!this.validDate(data.date)) {
-                    this.shiftForm.addError('date', 'Required');
-                }
-                
                 if (!this.validDate(data.start_time)) {
-                    this.shiftForm.addError('start_time', 'Required');
+                    this.shiftForm.addError('start_time', 'Clock in time is required');
                 }
                 
                 if (!this.validDate(data.end_time)) {
-                    this.shiftForm.addError('end_time', 'Required');
+                    this.shiftForm.addError('end_time', 'Clock out time is required');
                 }
 
                 if (data.mileage === '' || isNaN(data.mileage)) {
-                    this.shiftForm.addError('mileage', 'Invalid');
+                    this.shiftForm.addError('mileage', 'Invalid entry');
                 }
                 
                 if (isNaN(data.other_expenses)) {
-                    this.shiftForm.addError('other_expenses', 'Invalid');
+                    this.shiftForm.addError('other_expenses', 'Invalid entry');
                 }
 
-                if (isNaN(data.caregiver_rate)) {
-                    this.shiftForm.addError('caregiver_rate', 'Invalid');
+                if (! data.activities || data.activities.length == 0) {
+                    this.shiftForm.addError('activities', 'You must select at least one activity');
                 }
+                
+                // if (isNaN(data.caregiver_rate)) {
+                //     this.shiftForm.addError('caregiver_rate', 'Invalid');
+                // }
 
-                if (isNaN(data.provider_fee)) {
-                    this.shiftForm.addError('provider_fee', 'Invalid');
-                }
+                // if (isNaN(data.provider_fee)) {
+                //     this.shiftForm.addError('provider_fee', 'Invalid');
+                // }
                 
                 return !this.shiftForm.hasError();
             },
@@ -255,10 +312,51 @@
             validTime() {
                 if (!val || val == '') return false;
                 return moment(this.value, 'hh:mm').isValid();
-            }
+            },
+
+            generateWeeks() {
+                let weeks = [];
+                var start = null;
+                var end = null;
+
+                for (var i = 0; i < 4; i++) {
+                    if (i > 0) {
+                        start = moment().subtract(i * 7, 'days').startOf('week');
+                        end = moment().subtract(i * 7, 'days').endOf('week');
+                    } else {
+                        start = moment().startOf('week');
+                        end = moment().endOf('week');
+                    }
+
+                    let w = {
+                       id: i,
+                       display: start.format('M/D/YYYY') + ' - ' + end.format('M/D/YYYY'),
+                       days: this.getDatesInRange(start, end),
+                    };
+                    weeks.push(w);
+                }
+
+                return weeks;
+            },
+
+            getDatesInRange(start, end) {
+                let days = [];
+                for (var i = 6; i >= 0; i--) {
+                    days.push(moment(end).subtract(i, 'days').format('YYYY-MM-DD'));                    
+                }
+                return days;
+            },
         },
 
         computed: {
+            modalTitle() {
+                if (! this.shiftForm.date) {
+                    return '';
+                }
+
+                return this.dow(this.shiftForm.date, true) + ' ' + moment(this.shiftForm.date).format('M/D/YYYY');
+            },
+
             isCaregiver() {
                 return this.caregiver.id ? true : false;
             },
@@ -309,8 +407,12 @@
                     this.client = {};
                 }
                 
-                this.shiftForm = new Form(this.newShift());
+                this.form.shifts = this.generateShiftsForWeek(this.form.week);
             },
+
+            'form.week': function(newVal, oldVal) {
+                this.form.shifts = this.generateShiftsForWeek(this.form.week);
+            }
         }
     }
 </script>
