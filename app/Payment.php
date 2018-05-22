9e89bf4694b89d9371ecc45c794f209ecb03d0f0
Devon <devon@jtrsolutions.com>
2018-05-22 11:16:05 -0400

    ALLY-331 Re-work care week calculation by using last occurring shift

--- app/Payment.php
+++ app/Payment.php
@@ -87,14 +87,12 @@ class Payment extends Model implements Auditable
 
     public function getWeekAttribute()
     {
-        if ($this->shifts()->exists()) {
-            $checked_in_time = optional($this->shifts()->first())->checked_in_time;
-            if (!is_null($checked_in_time)) {
-                return (object)[
-                    'start' => $checked_in_time->setIsoDate($checked_in_time->year, $checked_in_time->weekOfYear)->toDateString(),
-                    'end' => $checked_in_time->setIsoDate($checked_in_time->year, $checked_in_time->weekOfYear, 7)->toDateString()
-                ];
-            }
+        $shift = $this->shifts()->orderBy('checked_in_time', 'DESC')->first();
+        if ($shift && $time = $shift->checked_in_time) {
+            return (object) [
+                'start' => $time->setIsoDate($time->year, $time->weekOfYear)->toDateString(),
+                'end' => $time->setIsoDate($time->year, $time->weekOfYear, 7)->toDateString()
+            ];
         }
         return null;
     }
