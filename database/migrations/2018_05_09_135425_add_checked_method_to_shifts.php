141609a3b1ac3b7c3282b10fc1741baaa9862144
Devon <devon@jtrsolutions.com>
2018-05-24 15:04:01 -0400

    Temporary fix: Clear beginning of checked_method migration

--- database/migrations/2018_05_09_135425_add_checked_method_to_shifts.php
+++ database/migrations/2018_05_09_135425_add_checked_method_to_shifts.php
@@ -14,35 +14,6 @@ class AddCheckedMethodToShifts extends Migration
      */
     public function up()
     {
-        Schema::table('shifts', function (Blueprint $table) {
-            $table->string('checked_in_method')->default('Unknown')->after('checked_in');
-            $table->string('checked_out_method')->default('Unknown')->after('checked_in_number');
-        });
-
-        Shift::chunk(100, function($shifts) {
-            foreach ($shifts as $shift) {
-                if (!$shift->checked_in) {
-                    $shift->checked_in_method = Shift::METHOD_CONVERTED;
-                    $shift->checked_out_method = Shift::METHOD_CONVERTED;
-                }
-                else {
-                    if ($shift->checked_in_number) {
-                        $shift->checked_in_method = Shift::METHOD_TELEPHONY;
-                    }
-                    else {
-                        $shift->checked_in_method = Shift::METHOD_GEOLOCATION;
-                    }
-                    if ($shift->checked_out_number) {
-                        $shift->checked_out_method = Shift::METHOD_TELEPHONY;
-                    }
-                    else if ($shift->checked_out_latitude || $shift->checked_out_longitude) {
-                        $shift->checked_out_method = Shift::METHOD_GEOLOCATION;
-                    }
-                    // Leave other clock outs as unknown
-                }
-                $shift->save();
-            }
-        });
 
         Schema::table('shifts', function (Blueprint $table) {
             $table->dropColumn('checked_in');
