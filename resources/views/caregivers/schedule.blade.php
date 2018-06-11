5b5c2a5a9c5fb99acfe553b01ff3c3d4985a6da4
Devon <devon@jtrsolutions.com>
2018-06-11 13:49:52 -0400

    Replace clock out checkboxes with default checkboxes (with zoom)

--- resources/views/caregivers/schedule.blade.php
+++ resources/views/caregivers/schedule.blade.php
@@ -2,10 +2,6 @@
 
 @section('title', 'Schedule')
 
-@push('head')
-    <style> .large-checkbox { zoom: 1.25; } </style>
-@endpush
-
 @section('content')
     @if(isset($_GET['clocked_out']))
         <div class="row">
@@ -16,33 +12,6 @@
             </div>
         </div>
     @endif
-
-    <div class="row">
-        <div class="col-lg-12">
-
-            <h2>Checkbox Test</h2>
-            <div class="form-group">
-                <label class="custom-control custom-checkbox"  style="clear: left; float: left;">
-                    <input type="checkbox" class="custom-control-input">
-                    <span class="custom-control-indicator"></span>
-                    <span class="custom-control-description">Bootstrap Checkbox</span>
-                </label>
-            </div>
-            <div class="form-group">
-                <label  style="clear: left; float: left;">
-                    <input type="checkbox" class="large-checkbox">
-                    <span class="custom-control-description">Zoomed Checkbox</span>
-                </label>
-            </div>
-            <div class="form-group">
-                <label  style="clear: left; float: left;">
-                    <input type="checkbox">
-                    <span class="custom-control-description">Default Checkbox</span>
-                </label>
-            </div>
-
-        </div>
-    </div>
 
     <caregiver-schedule></caregiver-schedule>
 @endsection
