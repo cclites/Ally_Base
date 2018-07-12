4780d4a167069272b25b12820afa4e9550b54667
faze <jonwillard11@gmail.com>
2018-07-12 10:08:29 -0400

    Added methods to handle priorities on the model

--- app/EmergencyContact.php
+++ app/EmergencyContact.php
@@ -36,4 +36,68 @@ class EmergencyContact extends Model implements Auditable
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * Returns the next free number for priority in the order sequence.
+     *
+     * @return int
+     */
+    public static function getNextPriorityForUser($user_id)
+    {
+        return self::select(\DB::raw('coalesce(max(`priority`), 0) as max_priority'))
+            ->where('user_id', $user_id)
+            ->get()
+            ->first()
+            ->max_priority + 1;
+    }
+
+    /**
+     * Increases the priority value for all of the users contacts
+     * at the given index and above, while skipping the excluded contact ID.
+     * Used to shift priority down in order to raise the priority for a specific contact.
+     *
+     * @param \App\User $user_id
+     * @param int $priority
+     * @param int $exclude_id
+     * @return void
+     */
+    public static function shiftPriorityDownAt($user_id, $priority, $exclude_id)
+    {
+        $index = $priority;
+
+        self::where('user_id', $user_id)
+            ->where('priority', '>=', $priority)
+            ->where('id', '!=', $exclude_id)
+            ->orderBy('priority')
+            ->get()
+            ->each(function ($item, $key) use (&$index) {
+                $index = $index + 1;
+                $item->update(['priority' => $index]);
+            });
+    }
+
+    /**
+     * Decreases the priority value for all of the users contacts
+     * above the given index.  Used to bump the priority up when a 
+     * contact is deleted.
+     *
+     * @param \App\User $user_id
+     * @param int $priority
+     * @param int $exclude_id
+     * @return void
+     */
+    public static function shiftPriorityUpAt($user_id, $priority, $exclude_id)
+    {
+        $index = $priority;
+
+        self::where('user_id', $user_id)
+            ->where('priority', '>', $priority)
+            ->orderBy('priority')
+            ->where('id', '!=', $exclude_id)
+            ->get()
+            ->each(function ($item, $key) use (&$index) {
+                $item->update(['priority' => $index]);
+                $index = $index + 1;
+            });
+    }
 }
