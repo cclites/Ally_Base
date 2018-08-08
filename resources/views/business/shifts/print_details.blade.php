dcbfc4eb266fedd55aac39fb15e3281b0ff99e98
faze <jonwillard11@gmail.com>
2018-08-08 07:57:52 -0400

    Changed format of shifts print template and added goals

--- resources/views/business/shifts/print_details.blade.php
+++ resources/views/business/shifts/print_details.blade.php
@@ -1,15 +1,135 @@
 <div class="container-fluid" style="margin-top: 1rem;">
-    <div class="with-padding-bottom row">
+    <div class="row">
         <div class="col-sm-6">
-            <strong>Client</strong>
-            <br/>
-            {{ $shift->client->name }}
+            <!-- LEFT SIDE -->
+            <div class="with-padding-bottom">
+                <strong>Client</strong>
+                <br/>
+                {{ $shift->client->name }}
+            </div>
+            <div class="with-padding-bottom">
+                <strong>Clocked In Time</strong><br/>
+                {{ $shift->checked_in_time->setTimezone($timezone)->format('m/d/Y g:i A') }}
+            </div>
+            <div class="with-padding-bottom">
+                <strong>Mileage</strong><br/>
+                {{ $shift->mileage }}
+            </div>
+            <div class="with-padding-bottom">
+                <strong>Special Designation</strong><br>
+                @switch($shift->hours_type)
+                    @case('default')
+                    Regular
+                    @break
+                    @case('overtime')
+                    Overtime
+                    @break
+                    @case('holiday')
+                    Holiday
+                    @break
+                @endswitch
+            </div>
+            @if($shift->schedule && $shift->schedule->notes)
+                <div class="with-padding-bottom">
+                    <strong>Schedule Notes</strong><br/>
+                    {{ $shift->schedule->notes }}
+                </div>
+            @endif
+            <div class="with-padding-bottom">
+                <h4>Activities Performed</h4>
+                <div class="row">
+                    <div class="col-sm-12">
+                        @if($shift->activities->count() == 0)
+                            <p>
+                                No activities recorded
+                            </p>
+                        @else
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Code</th>
+                                    <th>Name</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($shift->activities as $activity)
+                                    <tr>
+                                        <td>{{ $activity->code }}</td>
+                                        <td>{{ $activity->name }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <!-- /end LEFT SIDE -->
         </div>
+
         <div class="col-sm-6">
-            <strong>Caregiver</strong><br/>
-            {{ $shift->caregiver->name }}
+            <!-- RIGHT SIDE -->
+            <div class="with-padding-bottom">
+                <strong>Caregiver</strong><br/>
+                {{ $shift->caregiver->name }}
+            </div>
+            <div class="with-padding-bottom">
+                <strong>Clocked Out Time</strong><br/>
+                {{ $shift->checked_out_time->setTimezone($timezone)->format('m/d/Y g:i A') }}<br/>
+            </div>
+            <div class="with-padding-bottom">
+                <strong>Other Expenses</strong><br/>
+                &dollar;{{ number_format($shift->other_expenses, 2) }}<br/>
+            </div>
+            @if($shift->other_expenses_desc)
+                <div class="with-padding-bottom">
+                    <strong>Other Expenses Description</strong><br/>
+                    {{ $shift->other_expenses_desc }}
+                </div>
+            @endif
+            <div class="with-padding-bottom">
+                <strong>Caregiver Comments</strong><br/>
+                {!! $shift->caregiver_comments ? nl2br($shift->caregiver_comments) : 'No comments recorded' !!}
+            </div>
+            <div class="with-padding-bottom">
+                <h4>Issues on Shift</h4>
+                <div class="row">
+                    <div class="col-sm-12">
+                        @if($shift->issues->count() == 0)
+                            <p>
+                                No issues reported
+                            </p>
+                        @else
+                            @foreach($shift->issues as $issue)
+                                <p>
+                                    @if($issue->caregiver_injury)
+                                        <strong>The caregiver reported an injury to themselves.<br/></strong>
+                                    @endif
+                                    {{ $issue->comments }}
+                                </p>
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+            </div>
+            @if($shift->goals->count() > 0)
+                <div class="with-padding-bottom">
+                    <h4>Goals</h4>
+                    @foreach($shift->goals as $goal)
+                    <div class="row with-padding-bottom" >
+                        <div class="col-sm-12">
+                            <strong>{{ $goal->question }}</strong><br/>
+                            {{ $goal->pivot->comments }}
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            @endif
+
+            <!-- /end RIGHT SIDE -->
         </div>
     </div>
+
     @if($shift->signature)
         <div class="row with-padding-bottom">
             <div>
@@ -20,110 +140,7 @@
             </div>
         </div>
     @endif
-    <div class="row with-padding-bottom">
-        <div class="col-sm-6">
-            <strong>Clocked In Time</strong><br/>
-            {{ $shift->checked_in_time->setTimezone($timezone)->format('m/d/Y g:i A') }}
-        </div>
-        <div class="col-sm-6">
-            <strong>Clocked Out Time</strong><br/>
-            {{ $shift->checked_out_time->setTimezone($timezone)->format('m/d/Y g:i A') }}<br/>
-        </div>
-    </div>
-    <div class="row with-padding-bottom">
-        <div class="col-sm-6">
-            <strong>Mileage</strong><br/>
-            {{ $shift->mileage }}
-        </div>
-        <div class="col-sm-6">
-            <strong>Other Expenses</strong><br/>
-            &dollar;{{ number_format($shift->other_expenses, 2) }}<br/>
-        </div>
-    </div>
-    @if($shift->other_expenses_desc)
-        <div class="row with-padding-bottom">
-            <div class="col-sm-12">
-                <strong>Other Expenses Description</strong><br/>
-                {{ $shift->other_expenses_desc }}
-            </div>
-        </div>
-    @endif
-    <div class="row">
-        <div class="col-sm-6 with-padding-bottom">
-            <strong>Special Designation</strong><br>
-            @switch($shift->hours_type)
-                @case('default')
-                Regular
-                @break
-                @case('overtime')
-                Overtime
-                @break
-                @case('holiday')
-                Holiday
-                @break
-            @endswitch
-        </div>
-    </div>
-    @if($shift->schedule && $shift->schedule->notes)
-        <div class="row with-padding-bottom">
-            <div class="col-sm-12">
-                <strong>Schedule Notes</strong><br/>
-                {{ $shift->schedule->notes }}
-            </div>
-        </div>
-    @endif
-    <div class="row with-padding-bottom">
-        <div class="col-sm-12">
-            <strong>Caregiver Comments</strong><br/>
-            {{ $shift->caregiver_comments ? $shift->caregiver_comments : 'No comments recorded' }}
-        </div>
-    </div>
-    <h4>Issues on Shift</h4>
-    <div class="row">
-        <div class="col-sm-12">
-            @if($shift->issues->count() == 0)
-                <p>
-                    No issues reported
-                </p>
-            @else
-                @foreach($shift->issues as $issue)
-                    <p>
-                        @if($issue->caregiver_injury)
-                            <strong>The caregiver reported an injury to themselves.<br/></strong>
-                        @endif
-                        {{ $issue->comments }}
-                    </p>
-                @endforeach
-            @endif
-        </div>
-    </div>
-    <h4>Activities Performed</h4>
-    <div class="row">
-        <div class="col-sm-12">
-            @if($shift->activities->count() == 0)
-                <p>
-                    No activities recorded
-                </p>
-            @else
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>Code</th>
-                        <th>Name</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($shift->activities as $activity)
-                        <tr>
-                            <td>{{ $activity->code }}</td>
-                            <td>{{ $activity->name }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            @endif
-        </div>
-    </div>
+
     <h4>EVV</h4>
     <div class="row">
         <div class="col-sm-12">
