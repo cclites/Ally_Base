86f8ff6e3f2927fc7f1e503f06e187f642cf270a
Devon <devon@jtrsolutions.com>
2018-08-10 17:29:42 -0400

    LF2/LF2) acknowledge existing shift exceptions and disable location exceptions by default

---
+++ database/migrations/2018_08_10_211946_change_default_exception_business_settings.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDefaultExceptionBusinessSettings extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('businesses', function(Blueprint $table) {
+            $table->boolean('location_exceptions')->default(0)->change();
+        });
+
+        if (config('app.env') !== 'local') {
+            \App\Business::query()->update(['location_exceptions' => 0]);
+            \App\SystemException::notAcknowledged()->where('reference_type', \App\Shift::class)->update(['acknowledged_at' => \Carbon\Carbon::now()]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
