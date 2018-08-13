826d7f6c7bc29e25257fd4608563b762d63041ae
Devon <devon@jtrsolutions.com>
2018-08-13 15:11:29 -0400

    Disable require signatures by default

---
+++ database/migrations/2018_08_13_185548_disable_require_signatures_by_default.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DisableRequireSignaturesByDefault extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->boolean('require_signatures')->default(0)->change();
+        });
+
+        if (config('app.env') === 'production') {
+            \App\Business::query()->update(['require_signatures' => 0]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('businesses', function (Blueprint $table) {
+            $table->boolean('require_signatures')->default(1)->change();
+        });
+    }
+}
