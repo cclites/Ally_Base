fc66861e485aa434058d7b506d70e9b4a3ba467a
faze <jonwillard11@gmail.com>
2018-08-24 15:35:03 -0400

    Finished attaching custom question answers on clock out

--- app/Question.php
+++ app/Question.php
@@ -25,4 +25,23 @@ class Question extends Model
     {
         return $this->belongsTo(Business::class);
     }
+
+    /**
+     * Filter the questions by client type.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param string $client_type
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeForType($query, $client_type)
+    {
+        if (empty($client_type)) {
+            return $query;
+        }
+
+        return $query->where(function($q) use ($client_type) {
+            $q->where('client_type', $client_type)
+                ->orWhereNull('client_type');
+        });
+    }
 }
