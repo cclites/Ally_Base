888bf7024e1a1ea06c6269085fa23b806bb797a9
Devon <devon@jtrsolutions.com>
2018-09-14 14:53:38 -0400

    Add user agent to check-my-time

--- resources/views/check-my-time.blade.php
+++ resources/views/check-my-time.blade.php
@@ -5,7 +5,8 @@
 @section('content')
     <div class="container text-center">
         <h5>
-            Your computer's time is set to: <span id="time" style="font-weight: bold"></span>
+            Your computer's time is set to: <span id="time" style="font-weight: bold"></span><br /><br />
+            Your computer's user agent is: <span id="userAgent" style="font-weight: bold"></span>
         </h5>
     </div>
 @endsection
@@ -13,5 +14,6 @@
 @push('scripts')
     <script>
         $('#time').text(moment().toDate());
+        $('#userAgent').text(window.navigator.userAgent);
     </script>
 @endpush
