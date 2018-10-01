10d3749ef2fa4ea5491d2161be9add6b1d49bf61
Devon <devon@jtrsolutions.com>
2018-10-01 15:07:11 -0400

    Add window.AuthUser for global Javascript access of the auth user details

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -19,3 +19,11 @@
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
+
+<script>
+    try {
+        window.gmapsKey = '{{ config('services.gmaps.key') }}';
+        window.AuthUser = JSON.parse('{!! auth()->user() !!}');
+    }
+    catch(e) { console.log(e); }
+</script>
