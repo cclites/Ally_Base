40be196087e13d3445777d7873bc558063f9bb73
Devon <devon@jtrsolutions.com>
2018-10-18 10:06:04 -0400

    Add client type descriptor field

--- app/Client.php
+++ app/Client.php
@@ -151,6 +151,7 @@ class Client extends Model implements UserRole, CanBeConfirmedInterface, Reconci
         'ltci_fax',
         'medicaid_id',
         'medicaid_diagnosis_codes',
+        'client_type_descriptor',
     ];
 
     ///////////////////////////////////////////
