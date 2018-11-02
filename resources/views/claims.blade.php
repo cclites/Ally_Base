b1ef77fd84469ca0bbe2901528651734e8f08722
FrenchMajesty <verdi.kap@gmail.com>
2018-11-01 19:50:26 -0500

    ALLY-610 : Create claims page

---
+++ resources/views/claims.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Coming Soon</div>
+
+                <div class="card-body">
+                    This page is not yet accessible, we apologize for the inconvenience.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
