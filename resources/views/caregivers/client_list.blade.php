c364824218f2eba3bfd6f55f1b89aa42e7b3ed0e
faze <jonwillard11@gmail.com>
2018-11-13 12:47:22 -0500

    Added breadcrumbs to caregivers clients index page

--- resources/views/caregivers/client_list.blade.php
+++ resources/views/caregivers/client_list.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', 'My Clients')
 
+@section('breadcrumbs')
+    <li class="breadcrumb-item"><a href="/">Home</a></li>
+    <li class="breadcrumb-item active">My Clients</li>
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-lg-12">
