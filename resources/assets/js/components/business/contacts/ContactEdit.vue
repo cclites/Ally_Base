fa0de73d66ca88021ddb53a2f359005ca35e3bbf
Verdi <verdi.kap@gmail.com>
2018-11-24 12:34:40 -0600

    ALLY-588: Correct broken form in `ContactEdit` component

--- resources/assets/js/components/business/contacts/ContactEdit.vue
+++ resources/assets/js/components/business/contacts/ContactEdit.vue
@@ -28,23 +28,23 @@
                         </b-form-input>
                         <input-help :form="form" field="lastname" text="Enter their last name."></input-help>
                     </b-form-group>
-                    <b-form-group label="Client Type" label-for="client_type">
-                        <b-form-select
-                                id="client_type"
-                                name="client_type"
-                                v-model="form.client_type"
-                        >
-                            <option value="">--Select--</option>
-                            <option value="private_pay">Private Pay</option>
-                            <option value="medicaid">Medicaid</option>
-                            <option value="VA">VA</option>
-                            <option value="LTCI">LTC Insurance</option>
-                        </b-form-select>
-                        <input-help :form="form" field="client_type" text="Select the type of payment the prospect will use."></input-help>
+                    <b-form-group label="Job Title" label-for="title">
+                        <b-form-input
+                            id="title"
+                            name="title"
+                            type="text"
+                            v-model="form.title"
+                        ></b-form-input>
+                        <input-help :form="form" field="title" text="Enter their job title."></input-help>
                     </b-form-group>
-                    <b-form-group label="Office Location" label-for="business_id">
-                        <business-location-select v-model="form.business_id"></business-location-select>
-                        <input-help :form="form" field="business_id" text="Select the type of payment the prospect will use."></input-help>
+                    <b-form-group label="Company" label-for="company">
+                        <b-form-input
+                            id="company"
+                            name="company"
+                            type="text"
+                            v-model="form.company"
+                        ></b-form-input>
+                        <input-help :form="form" field="company" text="Enter their company."></input-help>
                     </b-form-group>
                 </b-col>
                 <b-col lg="6">
@@ -54,20 +54,18 @@
                                 name="email"
                                 type="email"
                                 v-model="form.email"
-                                :disabled="form.no_email"
-                                @change="copyEmailToUsername()"
                         >
                         </b-form-input>
                         <input-help :form="form" field="email"
                                     text="Enter their email address. Ex: user@domain.com"></input-help>
                     </b-form-group>
-                    <b-form-group label="Date of Birth" label-for="date_of_birth">
-                        <mask-input v-model="form.date_of_birth" id="date_of_birth" type="date"></mask-input>
-                        <input-help :form="form" field="date_of_birth" text="Enter their date of birth. Ex: MM/DD/YYYY"></input-help>
-                    </b-form-group>
                     <b-form-group label="Phone Number" label-for="phone">
                         <mask-input v-model="form.phone" name="phone"></mask-input>
                         <input-help :form="form" field="phone" text="Enter full phone number."></input-help>
+                    </b-form-group>
+                    <b-form-group label="General Notes" label-for="general_notes">
+                        <b-form-textarea v-model="form.general_notes" id="general_notes" :rows="3"></b-form-textarea>
+                        <input-help :form="form" field="general_notes" text="You can use this input to enter some notes about this contact."></input-help>
                     </b-form-group>
                 </b-col>
             </b-row>
@@ -130,19 +128,6 @@
             </b-row>
             <hr />
             <b-row>
-                <b-col lg="6">
-                    <referral-source-select v-model="form.referral_source_id" :business-id="form.business_id"></referral-source-select>
-                    <input-help :form="form" field="referred_by" text="Enter how the prospect was referred." />
-                </b-col>
-                <b-col lg="6">
-                    <b-form-group label="Last Contacted" label-for="last_contacted">
-                        <date-picker v-model="form.last_contacted"></date-picker>
-                        <input-help :form="form" field="last_contacted" text="Enter when the prospect was contacted." />
-                    </b-form-group>
-                </b-col>
-            </b-row>
-            <hr />
-            <b-row>
                 <b-col lg="12">
                     <submit-button variant="success"
                                    type="submit"
@@ -160,12 +145,9 @@
 <script>
     import Countries from "../../../classes/Countries";
     import States from "../../../classes/States";
-    import ReferralSourceSelect from "../referral/ReferralSourceSelect";
-    import BusinessLocationSelect from "../BusinessLocationSelect";
 
     export default {
-        components: {BusinessLocationSelect, ReferralSourceSelect},
-        props: ['contact', 'referralsources'],
+        props: ['contact'],
 
         data() {
             return {
@@ -175,7 +157,8 @@
                     'email': this.getOriginal('email'),
                     'client_type': this.getOriginal('client_type'),
                     'business_id': this.getOriginal('business_id'),
-                    'date_of_birth': this.getOriginalDate('date_of_birth'),
+                    'title': this.getOriginalDate('title'),
+                    'company': this.getOriginalDate('company'),
                     'phone': this.getOriginal('phone'),
                     'address1': this.getOriginal('address1'),
                     'address2': this.getOriginal('address2'),
@@ -183,18 +166,7 @@
                     'state': this.getOriginal('state'),
                     'zip': this.getOriginal('zip'),
                     'country': this.getOriginal('country', 'US'),
-                    'referral_source_id': this.getOriginal('referral_source_id'),
-                    'last_contacted': this.getOriginalDate('last_contacted'),
-                    'initial_call_date': this.getOriginalDate('initial_call_date'),
-                    'had_initial_call': this.getOriginal('had_initial_call', 0),
-                    'had_assessment_scheduled': this.getOriginal('had_assessment_scheduled', 0),
-                    'had_assessment_performed': this.getOriginal('had_assessment_performed', 0),
-                    'needs_contract': this.getOriginal('needs_contract', 0),
-                    'expecting_client_signature': this.getOriginal('expecting_client_signature', 0),
-                    'needs_payment_info': this.getOriginal('needs_payment_info', 0),
-                    'ready_to_schedule': this.getOriginal('ready_to_schedule', 0),
-                    'closed_loss': this.getOriginal('closed_loss', 0),
-                    'closed_win': this.getOriginal('closed_win', 0),
+                    'general_notes': this.getOriginal('general_notes'),
                 }),
                 statuses: {
                     'Assessment Scheduled': 'had_assessment_scheduled',
