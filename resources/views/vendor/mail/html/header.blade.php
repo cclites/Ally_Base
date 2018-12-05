740a4d18e8bdd6ef747c828e0a0a14b21166fb0b
Devon <devon@jtrsolutions.com>
2018-12-05 10:45:09 -0500

    Use URL to logo instead of embedding

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="{{ $message->embed(public_path('images/AllyLogo-new-light.png')) }}">
+            <img src="{{ asset('images/AllyLogo-new-light.png') }}">
         </a>
     </td>
 </tr>
