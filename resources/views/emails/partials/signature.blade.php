ecb2b9bb38420fb6b287e2331ea61f038f88aa85
Devon <devon@jtrsolutions.com>
2018-12-05 10:44:40 -0500

    Use URL to logo instead of embedding

--- resources/views/emails/partials/signature.blade.php
+++ resources/views/emails/partials/signature.blade.php
@@ -1,6 +1,6 @@
 Sincerely,
 
-<img src="{{ $message->embed(public_path('images/AllyLogo-new-light.png')) }}" style="max-height: 50px">
+<img src="{{ asset('images/AllyLogo-new-light.png') }}" style="max-height: 50px">
 
 (800) 930-0587
 support@allyms.com
