9169050503a9e2f38c8353581bd742470e3e428e
Verdi <verdi.kap@gmail.com>
2018-12-06 16:21:55 -0600

    ALLY-622: Create `CustomFieldController` and wrote the `create` method

--- routes/web.php
+++ routes/web.php
@@ -120,6 +120,7 @@ Route::group([
 ], function() {
     Route::resource('activities', 'Business\ActivityController')->only(['index', 'store', 'update', 'destroy']);
 
+    Route::resource('custom-fields', 'Business\CustomFieldController');
     Route::get('settings/bank-accounts/{business?}', 'Business\SettingController@bankAccounts')->name('settings.bank_accounts.index');
     Route::post('settings/bank-account/{type}', 'Business\SettingController@storeBankAccount')->name('settings.bank_accounts.update');
     Route::get('settings', 'Business\SettingController@index')->name('settings.index');
