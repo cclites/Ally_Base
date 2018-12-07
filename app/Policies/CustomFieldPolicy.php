e2f4969393ca49968b813139d272007327f3075a
Verdi <verdi.kap@gmail.com>
2018-12-06 18:14:25 -0600

    ALLY-622: Create `CustomFieldPolicy` policy and `UpdateCustomFieldRequest` request

---
+++ app/Policies/CustomFieldPolicy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\CustomField;
+
+class CustomFieldPolicy extends BasePolicy
+{
+    public function create(User $user, $data)
+    {
+        $client = new CustomField($data);
+        return $this->businessCheck($user, $field);
+    }
+
+    public function read(User $user, CustomField $field)
+    {
+        return $this->businessCheck($user, $field);
+    }
+
+    public function update(User $user, CustomField $field)
+    {
+        return $this->businessCheck($user, $field);
+    }
+
+    public function delete(User $user, CustomField $field)
+    {
+        return $this->businessCheck($user, $field);
+    }
+}
