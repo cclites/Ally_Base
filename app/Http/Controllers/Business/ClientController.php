f8ec27b247ec3a3aa52378fa350bc835cc1f4b01
Verdi <verdi.kap@gmail.com>
2018-12-11 12:47:34 -0600

    ALLY-622: Add `meta` attribute relation in `ClientController@show` method

--- app/Http/Controllers/Business/ClientController.php
+++ app/Http/Controllers/Business/ClientController.php
@@ -155,6 +155,7 @@ class ClientController extends BaseController
             'creditCards',
             'payments',
             'user.documents',
+            'meta',
             'notes.creator',
             'careDetails',
             'carePlans',
