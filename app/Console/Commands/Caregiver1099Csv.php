1cb72ff5eb6a8ce6434fbf514a6732f507d76d7c
Devon <devon@jtrsolutions.com>
2018-12-17 17:19:50 -0500

    Update 1099 CSV command

--- app/Console/Commands/Caregiver1099Csv.php
+++ app/Console/Commands/Caregiver1099Csv.php
@@ -14,7 +14,7 @@ class Caregiver1099Csv extends Command
      *
      * @var string
      */
-    protected $signature = 'csv:caregiver_1099 {--output=}';
+    protected $signature = 'csv:caregiver_1099 {year?} {--output=}';
 
     /**
      * The console command description.
@@ -29,9 +29,9 @@ class Caregiver1099Csv extends Command
     protected $csvSeparator = ',';
 
     /**
-     * @var int
+     * @var int|float
      */
-    protected $year;
+    protected $minimumThreshold = 650;
 
 
     /**
@@ -42,7 +42,6 @@ class Caregiver1099Csv extends Command
     public function __construct()
     {
         parent::__construct();
-        $this->year = date('Y') - 1;
     }
 
     /**
@@ -53,48 +52,35 @@ class Caregiver1099Csv extends Command
     public function handle()
     {
         $rows = [];
+        $year = (int) $this->argument('year') ?? date('Y');
 
-        // Get clients that are LTCI or Provider Pay
-        $clients = Client::where('client_type', 'LTCI')
-            ->orWhere('default_payment_type', '=', 'App\\Business')
-            ->get();
+        // Disable full group by mode
+        \DB::statement('set session sql_mode=\'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION\';');
 
-        foreach($clients as $client) {
-            $shifts = $client->shifts()
-                             ->with('caregiver')
-                             ->whereHas('deposits', function($q) {
-                                 $q->where('deposit_type', 'caregiver')
-                                   ->whereYear('created_at', $this->year);
-                             })->get();
+        // Get rows
+        $rows = \DB::select("SELECT c.id as client_id, CONCAT(u1.firstname, ' ', u1.lastname) as client_name, u1.email as client_email, c.client_type, c.default_payment_type, c.ssn as client_ssn, 
+a1.address1 as client_address1, a1.address2 as client_address2, a1.city as client_city, a1.state as client_state, a1.zip as client_zip,
+u2.id as caregiver_id, CONCAT(u2.firstname, ' ', u2.lastname) as caregiver_name, u2.email as caregiver_email, c2.ssn as caregiver_ssn,
+a2.address1 as caregiver_address1, a2.address2 as caregiver_address2, a2.city as caregiver_city, a2.state as caregiver_state, a2.zip as caregiver_zip,
+ sum(h.caregiver_total) as payment_total
+FROM clients c
+INNER JOIN shifts s ON s.client_id = c.id
+INNER JOIN payments p ON s.payment_id = p.id
+INNER JOIN shift_cost_history h ON h.id = s.id
+INNER JOIN users u1 ON u1.id = s.client_id
+INNER JOIN users u2 ON u2.id = s.caregiver_id
+INNER JOIN caregivers c2 ON c2.id = u2.id
+LEFT JOIN addresses a1 ON a1.id = (SELECT id FROM addresses WHERE user_id = u1.id ORDER BY `type` LIMIT 1)
+LEFT JOIN addresses a2 ON a2.id = (SELECT id FROM addresses WHERE user_id = u2.id ORDER BY `type` LIMIT 1)
+WHERE p.created_at BETWEEN '{$year}-01-01 00:00:00' AND '{$year}-12-31 23:59:59'
+GROUP BY s.client_id, s.caregiver_id
+HAVING payment_total > ?", [$this->minimumThreshold]);
 
-            if (!$shifts->count()) {
-                continue;
-            }
-
-            foreach($shifts->groupBy('caregiver_id') as $caregiverId => $caregiverShifts) {
-                $caregiver = $caregiverShifts->first()->caregiver;
-                $amount = 0;
-                foreach($caregiverShifts as $shift) {
-                    $amount = bcadd($amount, $shift->costs()->getCaregiverCost(), 2);
-                }
-                $address = $caregiver->addresses->first();
-                $rows[] = [
-                    'client_id' => $client->id,
-                    'client_name' => $client->name(),
-                    'caregiver_id' => $caregiver->id,
-                    'caregiver_name' => $caregiver->name(),
-                    'amount' => $amount,
-                    'ssn' => $caregiver->ssn,
-                    'address' => $address->address1,
-                    'city' => $address->city,
-                    'state' => $address->state,
-                    'zip' => $address->zip,
-                ];
-            }
-        }
-
-        $csv = implode($this->csvSeparator, array_keys($rows[0])) . "\n";
+        $csv = implode($this->csvSeparator, array_keys((array) $rows[0])) . "\n";
         foreach($rows as $row) {
+            $row = (array) $row;
+            $row['client_ssn'] = $row['client_ssn'] ? \Crypt::decrypt($row['client_ssn']) : null;
+            $row['caregiver_ssn'] = $row['caregiver_ssn'] ? \Crypt::decrypt($row['caregiver_ssn']) : null;
             $csv .= implode($this->csvSeparator, $row) . "\n";
         }
 
