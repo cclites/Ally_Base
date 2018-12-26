426163d0a6e0d08f4c5156d060caf6975dc8ff29
Devon <devon@jtrsolutions.com>
2018-12-26 13:09:55 -0500

    Fix missing CC types

---
+++ tests/Bugs/AllyMissingCCType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Bugs;
+
+use App\CreditCard;
+use Tests\TestCase;
+
+class AllyMissingCCType extends TestCase
+{
+
+    public function test_a_credit_card_type_is_set_when_a_cc_number_is_entered()
+    {
+        /**
+         * @var CreditCard $card
+         */
+        $card = factory(CreditCard::class)->make();
+        $card->type = null;
+
+        $card->number = '4111111111111111';
+        $this->assertEquals('visa', $card->type);
+
+        $card->number = '378734493671000';
+        $this->assertEquals('amex', $card->type);
+    }
+
+}
