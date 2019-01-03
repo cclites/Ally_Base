0e2d45d735cbde2a90b030191d089fc1aaca6ae5
faze <jonwillard11@gmail.com>
2019-01-03 11:06:08 -0500

    Added phone number validation to notification options ALLY-714

--- app/Http/Requests/UpdateNotificationOptionsRequest.php
+++ app/Http/Requests/UpdateNotificationOptionsRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use App\Rules\PhonePossible;
 
 class UpdateNotificationOptionsRequest extends FormRequest
 {
@@ -29,10 +30,15 @@ class UpdateNotificationOptionsRequest extends FormRequest
             'allow_email_notifications' => 'required|boolean',
             'allow_system_notifications' => 'required|boolean',
             'notification_email' => 'nullable',
-            'notification_phone' => 'nullable',
+            'notification_phone' => ['nullable', new PhonePossible()],
         ];
     }
 
+    /**
+     * Get the validator instance for the request.
+     *
+     * @return \Illuminate\Validation\Validator
+     */
     protected function getValidatorInstance() {
         $validator = parent::getValidatorInstance();
     
