1d8da7cf24a66b949cce4cad06cbf8a5903d4b74
Devon <devon@jtrsolutions.com>
2019-01-08 09:06:04 -0500

    Update service model

--- app/Billing/Service.php
+++ app/Billing/Service.php
@@ -5,13 +5,30 @@ use App\AuditableModel;
 use App\Contracts\BelongsToChainsInterface;
 use App\Traits\BelongsToOneChain;
 
+/**
+ * \App\Billing\Service
+ *
+ * @property int $id
+ * @property int $chain_id
+ * @property bool $default
+ * @property-read \Illuminate\Database\Eloquent\Collection|\OwenIt\Auditing\Models\Audit[] $audits
+ * @property-read \App\BusinessChain $businessChain
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Billing\Service forAuthorizedChain(\App\User $authorizedUser = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Billing\Service forChains($chains)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\BaseModel ordered($direction = null)
+ * @mixin \Eloquent
+ */
 class Service extends AuditableModel implements BelongsToChainsInterface
 {
     use BelongsToOneChain;
 
+    protected $orderedColumn = 'name';
+
     protected $fillable = ['name', 'default', 'chain_id'];
+
     protected $casts = [
-        'default' => 'boolean'
+        'default' => 'bool',
+        'chain_id' => 'int',
     ];
 
     /**
