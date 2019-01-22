4cef6673e30ba84d60f56d1e0dfb2e4c5afd7d9c
faze <jonwillard11@gmail.com>
2019-01-22 14:45:09 -0500

    Refactored and optimized query for Shift Summary Email CRON
    - Run report on one business at a time
    - Filter clients based on profile settings at a query level
    - Re-name forEmail() method
    - Pre-load all relationships required for cost calculations, preventing n+1 issue

--- app/Console/Commands/CronShiftSummaryEmails.php
+++ app/Console/Commands/CronShiftSummaryEmails.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Str;
 use App\ShiftConfirmationToken;
 use App\ShiftConfirmation;
 use App\Reports\UnconfirmedShiftsReport;
+use App\Business;
+use App\Client;
 
 class CronShiftSummaryEmails extends Command
 {
@@ -41,43 +43,78 @@ class CronShiftSummaryEmails extends Command
      *
      * @return mixed
      */
-    public function handle(UnconfirmedShiftsReport $report)
+    public function handle()
     {
-        $unconfirmedShifts = $report->includeConfirmed()
-            ->includeClockedIn()
-            ->maskNames()
-            ->forEmail()
-            ->rows()
-            ->groupBy('client_id');
+        foreach ($this->getIncludedBusinessIds() as $business) {
+            $clients = $this->getIncludedClientIds($business);
 
-        foreach ($unconfirmedShifts as $client_id => $shifts) {
-            $client = $shifts->first()->client;
-            if (! $client->receive_summary_email) {
-                // skip clients that have their weekly summary emails turned off
+            if (empty($clients)) {
                 continue;
             }
 
-            $businessName = $shifts->first()->business_name;
-            $total = $shifts->sum('total');
+            $report = new UnconfirmedShiftsReport();
+            $shifts = $report->includeConfirmed()
+                ->includeClockedIn()
+                ->includeInProgress()
+                ->forBusinesses($business)
+                ->forClients($clients)
+                ->maskNames()
+                ->rows()
+                ->groupBy('client_id');
 
-            $confirmation = ShiftConfirmation::create([
-                'client_id' => $client->id,
-                'token' => Str::random(64),
-            ]);
-            $confirmation->shifts()->sync($shifts->pluck('id'));
+            foreach ($shifts as $client_id => $shifts) {
+                $client = $shifts->first()->client;
+                $businessName = $shifts->first()->business_name;
+                $total = $shifts->sum('total');
 
-            if ($email = filter_var($client->email, FILTER_VALIDATE_EMAIL)) {
-                \Mail::to($client->email)->send(new ClientShiftSummaryEmail(
-                    $client,
-                    $shifts,
-                    $total,
-                    $businessName,
-                    $confirmation->token
-                ));
-                sleep(1); // sleep 1s after each email
+                $confirmation = ShiftConfirmation::create([
+                    'client_id' => $client->id,
+                    'token' => Str::random(64),
+                ]);
+                $confirmation->shifts()->sync($shifts->pluck('id'));
+
+                if ($email = filter_var($client->email, FILTER_VALIDATE_EMAIL)) {
+                    \Mail::to($client->email)->send(new ClientShiftSummaryEmail(
+                        $client,
+                        $shifts,
+                        $total,
+                        $businessName,
+                        $confirmation->token
+                    ));
+                    sleep(1); // sleep 1s after each email
+                }
+                
+                // break; // <----------------- for testing
             }
-            
-            // break; // <----------------- for testing
         }
+    }
+
+    /**
+     * Get the businesses that are set up to send shift confirmation emails.
+     * Returns an empty array if report is not for email.
+     *
+     * @return array
+     */
+    public function getIncludedBusinessIds()
+    {
+        return Business::where('shift_confirmation_email', true)
+            ->get()
+            ->pluck('id')
+            ->toArray();
+    }
+
+    /**
+     * Filter the clients to only those that have their 
+     * weekly summary emails turned ON.
+     *
+     * @return array
+     */
+    public function getIncludedClientIds($business)
+    {
+        return Client::where('business_id', $business)
+                ->where('receive_summary_email', 1)
+                ->get()
+                ->pluck('id')
+                ->toArray();
     }
 }
