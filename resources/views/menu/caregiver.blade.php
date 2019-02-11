2ee41c4a528b11254bb53019361b1a73f9ebd8ff
Devon <devon@jtrsolutions.com>
2019-02-11 13:49:48 -0500

    Keep clock in link in the menu to allow for multiple clock ins

--- resources/views/menu/caregiver.blade.php
+++ resources/views/menu/caregiver.blade.php
@@ -1,10 +1,11 @@
 <li>
-    @if(auth()->user()->role->isClockedIn())
-        <a class="has-arrow" href="{{ route('clock_out') }}" aria-expanded="false"><i class="fa fa-calendar-check-o"></i><span class="hide-menu">Clock Out</span></a>
-    @else
-        <a class="has-arrow" href="{{ route('clock_in') }}" aria-expanded="false"><i class="fa fa-calendar-check-o"></i><span class="hide-menu">Clock In</span></a>
-    @endif
+    <a class="has-arrow" href="{{ route('clock_in') }}" aria-expanded="false"><i class="fa fa-calendar-check-o"></i><span class="hide-menu">Clock In</span></a>
 </li>
+@if(auth()->user()->role->isClockedIn())
+    <li>
+        <a class="has-arrow" href="{{ route('clock_out') }}" aria-expanded="false"><i class="fa fa-calendar-check-o"></i><span class="hide-menu">Clock Out</span></a>
+    </li>
+@endif
 <li>
     <a class="has-arrow" href="{{ route('schedule') }}" aria-expanded="false"><i class="fa fa-calendar"></i><span class="hide-menu">Schedule</span></a>
 </li>
