8b3157de3e7b656f8395ad75b241fa5d94dcd141
Devon <devon@jtrsolutions.com>
2019-02-12 21:06:01 -0500

    Chunk shifts migration

--- database/migrations/2019_02_07_201049_migrate_existing_shifts_to_new_structure.php
+++ database/migrations/2019_02_07_201049_migrate_existing_shifts_to_new_structure.php
@@ -20,32 +20,21 @@ class MigrateExistingShiftsToNewStructure extends Migration
         DB::beginTransaction();
 
         ////////////////////////////////////
-        //// Migrate existing pending shifts
+        //// Migrate existing shifts
         ////////////////////////////////////
 
-        $statuses = \App\Shifts\ShiftStatusManager::getPendingStatuses() + [\App\Shift::WAITING_FOR_CHARGE];
-        $shifts = \App\Shift::with(['client', 'client.defaultPayment'])->whereIn('status', $statuses)->get();
         $count = 0;
-        $shifts->each(function (\App\Shift $shift) use (&$count) {
-            $status = $shift->status === \App\Shift::WAITING_FOR_CHARGE ? \App\Shift::WAITING_FOR_INVOICE : $shift->status;
-            $rate = $shift->costs()->getTotalHourlyCost();
-            $count += \DB::table('shifts')->where('id', $shift->id)->update([
-                'client_rate' => $rate,
-                'status' => $status,
-            ]);
+        \App\Shift::with(['client', 'client.defaultPayment'])->chunk(1000, function($shifts) {
+            $shifts->each(function (\App\Shift $shift) use (&$count) {
+                $status = $shift->status === \App\Shift::WAITING_FOR_CHARGE ? \App\Shift::WAITING_FOR_INVOICE : $shift->status;
+                $rate = $shift->costs()->getTotalHourlyCost();
+                $count += \DB::table('shifts')->where('id', $shift->id)->update([
+                    'client_rate' => $rate,
+                    'status' => $status,
+                ]);
+            });
         });
 
-        ////////////////////////////////////
-        //// Migrate all other shifts
-        ////////////////////////////////////
-
-        $shifts = \App\Shift::with(['client', 'client.defaultPayment'])->whereNotIn('status', $statuses)->get();
-        $shifts->each(function (\App\Shift $shift) use (&$count) {
-            $rate = $shift->costs()->getTotalHourlyCost();
-            $count += \DB::table('shifts')->where('id', $shift->id)->update([
-                'client_rate' => $rate,
-            ]);
-        });
 
         ////////////////////////////////////
         //// Migrate polymorphic relations
