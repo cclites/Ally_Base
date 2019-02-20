085533ca7964f5e67bcf6de255bcabfe04fa3eb8
Devon <devon@jtrsolutions.com>
2019-02-20 09:39:41 -0500

    Create a default payer when a client is created - Clean up client creation logic, encapsulate in a CreateClient action

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Events\BusinessChainCreated;
+use App\Events\ClientCreated;
 use App\Events\FailedTransactionFound;
 use App\Events\FailedTransactionRecorded;
 use App\Events\ShiftCreated;
@@ -12,6 +13,7 @@ use App\Events\UnverifiedShiftCreated;
 use App\Events\UnverifiedShiftLocation;
 use App\Listeners\AddPaymentHoldsOnFailedTransaction;
 use App\Listeners\CheckForClockOut;
+use App\Listeners\CreateDefaultClientPayer;
 use App\Listeners\CreateDefaultService;
 use App\Listeners\PostToSlackOnFailedTransaction;
 use App\Listeners\ShiftStatusUpdate;
@@ -39,6 +41,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         BusinessChainCreated::class => [
             CreateDefaultService::class,
+        ],
+        ClientCreated::class => [
+            CreateDefaultClientPayer::class,
         ],
         TaskAssigned::class => [
 //            SendAssignedTaskEmail::class,
