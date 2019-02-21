94e2d94e9388860e5bfc4fd865da3aef7ceadb2c
faze <jonwillard11@gmail.com>
2019-02-21 09:39:06 -0500

    wip

--- resources/assets/js/components/account-setup/ClientSetupWizard.vue
+++ resources/assets/js/components/account-setup/ClientSetupWizard.vue
@@ -19,7 +19,17 @@
             header="Your account is set up!"
             header-bg-variant="info"
             header-text-variant="white"
-        ></b-card>
+        >
+            <p>You have successfully onboarded.  Please use your username and password to login to our system to manage your details and view invoice and payment history.</p>
+
+            <p>If you forgot your username, please contact (800)-930-0587</p>
+
+            <p>
+                <b-btn href="/login" class="success">Login Now</b-btn>
+            </p>
+
+            <p>For FAQs and other training material, please go to our <a href="/knowledge-base">Knowledge Base</a>.</p>
+        </b-card>
     </div>
 </template>
 
