5a8bf39ea735d849d2927aea15994f729ce4685b
faze <jonwillard11@gmail.com>
2019-03-04 11:28:35 -0500

    Remove business field from communications log ALLY-802

--- database/migrations/2019_02_28_212020_create_communication_logs_table.php
+++ database/migrations/2019_02_28_212020_create_communication_logs_table.php
@@ -15,8 +15,6 @@ class CreateCommunicationLogsTable extends Migration
     {
         Schema::create('communication_logs', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('business_id')->nullable();
-//            $table->unsignedInteger('user_id');
             $table->string('channel')->index();
             $table->string('subject')->nullable();
             $table->text('body');
@@ -24,9 +22,6 @@ class CreateCommunicationLogsTable extends Migration
             $table->string('from')->nullable();
             $table->timestamp('sent_at')->nullable();
             $table->timestamps();
-
-            $table->foreign('business_id')->references('id')->on('businesses')->onUpdate('CASCADE')->onDelete('CASCADE');
-//            $table->foreign('user_id')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
