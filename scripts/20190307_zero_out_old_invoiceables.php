4e0658397e8062aa13f31b921cdd4cb3bde8e8d7
Devon <devon@jtrsolutions.com>
2019-03-07 13:43:35 -0500

    Add fixed script to zero out old $0 provider fee shifts

---
+++ scripts/20190307_zero_out_old_invoiceables.php
@@ -0,0 +1,43 @@
+<?php
+
+require __DIR__ . '/bootstrap.php';
+
+////////////////////////////////////
+//// Clear out old $0 provider fee shifts (add them to a new invoice that should just be $0)
+////////////////////////////////////
+
+foreach(\App\Business::all() as $business) {
+    $query = new \App\Billing\Queries\InvoiceableQuery(new \App\Shift());
+    $shifts = $query->forBusinesses([$business->id])
+        ->hasClientInvoicesPaid()
+        ->doesntHaveBusinessInvoice()
+        ->whereIn('status', ['PAID', 'PAID_CAREGIVER_ONLY'])
+        ->where('provider_fee', '0')
+        ->get();
+
+    if ($shifts->count()) {
+        $invoice = \App\Billing\BusinessInvoice::create([
+            'name' => \App\Billing\BusinessInvoice::getNextName($business->id),
+            'business_id' => $business->id,
+            'created_at' => '2019-03-06 00:00:00',
+        ]);
+
+        /** @var \App\Shift $shift */
+        foreach($shifts as $shift) {
+            $item = new \App\Billing\BusinessInvoiceItem([
+                'group' => $shift->getItemGroup(\App\Billing\BusinessInvoice::class),
+                'name' => $shift->getItemName(\App\Billing\BusinessInvoice::class),
+                'units' => $shift->duration(),
+                'client_rate' => $clientRate = $shift->costs()->getTotalHourlyCost(),
+                'caregiver_rate' => $shift->caregiver_rate,
+                'ally_rate' => subtract($clientRate, add($shift->caregiver_rate, $shift->provider_fee)),
+                'rate' => $shift->provider_fee,
+                'total' => $shift->costs()->getProviderFee(),
+                'date' => $shift->getItemDate(),
+            ]);
+            $item->associateInvoiceable($shift);
+            $invoice->addItem($item);
+        }
+    }
+
+}
