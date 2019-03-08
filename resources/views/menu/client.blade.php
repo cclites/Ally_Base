4c21c7817a81ab81c769b0f40f7ac47d50c66520
faze <jonwillard11@gmail.com>
2019-03-08 09:58:08 -0500

    Add all users menu items for knowledge base ALLY-950

--- resources/views/menu/client.blade.php
+++ resources/views/menu/client.blade.php
@@ -32,8 +32,8 @@
         <i class="fa fa-users"></i><span class="hide-menu">My Caregivers</span>
     </a>
 </li>
-{{--<li>--}}
-    {{--<a class="" href="{{ route('knowledge.base') }}" aria-expanded="false">--}}
-        {{--<i class="fa fa-lightbulb-o"></i><span class="hide-menu">Knowledge Base</span>--}}
-    {{--</a>--}}
-{{--</li>--}}
+<li>
+    <a class="" href="{{ route('knowledge.base') }}" aria-expanded="false">
+        <i class="fa fa-lightbulb-o"></i><span class="hide-menu">Knowledge Base</span>
+    </a>
+</li>
