6c47a50542a6b350242c5cc1d9548e54d011ec9a
Devon <devon@jtrsolutions.com>
2019-05-10 12:15:35 -0400

    Move long running migration to a script

---
+++ scripts/20190510_progress_existing_shift_statuses.php
@@ -0,0 +1,37 @@
+<?php
+require __DIR__ . '/bootstrap.php';
+
+\DB::beginTransaction();
+$query = (new InvoiceableQuery(new Shift()))->notBelongingToAnOldFinalizedShift();
+$shifts = (clone $query)->hasClientInvoice()->get();
+/** @var Shift $shift */
+foreach($shifts as $shift) {
+    $shift->statusManager()->ackClientInvoice();
+}
+echo $shifts->count() . " acknowledged client invoice.\n";
+
+$shifts = (clone $query)->hasClientInvoicesPaid()->doesntHaveCaregiverInvoice()->get();
+/** @var Shift $shift */
+foreach($shifts as $shift) {
+    $shift->statusManager()->ackPayment();
+
+}
+echo $shifts->count() . " acknowledged client payment.\n";
+
+
+$shifts = (clone $query)->hasClientInvoicesPaid()->hasCaregiverInvoicesPaid()->hasBusinessInvoicesPaid()->get();
+/** @var Shift $shift */
+foreach($shifts as $shift) {
+    $shift->statusManager()->update(Shift::PAID);
+}
+echo $shifts->count() . " updated to PAID.\n";
+
+$query = new InvoiceableQuery(new ShiftService());
+$shiftServices = (clone $query)->hasClientInvoicesPaid()->hasCaregiverInvoicesPaid()->hasBusinessInvoicesPaid()->get();
+/** @var ShiftService $shiftService */
+foreach($shiftServices as $shiftService) {
+    $shiftService->getShift()->statusManager()->update(Shift::PAID);
+}
+echo $shiftServices->count() . " related services updated to PAID.\n";
+
+\DB::commit();
