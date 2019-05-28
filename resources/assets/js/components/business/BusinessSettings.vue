2bd9c95891b091afde3be7db1ebcdc35a803b964
faze <jonwillard11@gmail.com>
2019-05-28 13:44:47 -0400

    Add disclaimer to business clock out questions settings tab ALLY-1193

--- resources/assets/js/components/business/BusinessSettings.vue
+++ resources/assets/js/components/business/BusinessSettings.vue
@@ -252,6 +252,11 @@
                 </b-tab>
                 <b-tab title="Clock Out Questions" href="#questions">
                     <b-row>
+                        <b-col md="12">
+                            <b-alert show variant="info">
+                                Note: These options only affect our mobile app.  Any changes here will not be reflected on your telephony system.
+                            </b-alert>
+                        </b-col>
                         <b-col lg="12">
                             <h4>Options</h4>
                             <hr/>
