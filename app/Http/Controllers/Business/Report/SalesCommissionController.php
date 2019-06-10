ea6adb7f58e465871f57b12d7498ac1c943067b2
Chad Clites <chad@extant.digital>
2019-06-10 15:53:02 -0500

    adding files back to proper branch

---
+++ app/Http/Controllers/Business/Report/SalesCommissionController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers\Business\Report;
+
+use App\Caregiver;
+use App\Client;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
---
+++ resources/assets/js/components/business/reports/SalesCommissionReport.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "SalesCommissionReport"
+    }
+</script>
+
+<style scoped>
+
+</style>
