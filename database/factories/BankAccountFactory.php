24578f31a6745f75c6d2ffb3d1608d834ab7da0e
faze <jonwillard11@gmail.com>
2019-06-17 11:09:15 -0400

    Add strict to routing number generator
    This was failing tests on occasion

--- database/factories/BankAccountFactory.php
+++ database/factories/BankAccountFactory.php
@@ -11,7 +11,7 @@ $factory->define(BankAccount::class, function (Faker $faker) {
         'business_id' => factory(Business::class)->create()->id,
         'nickname' => $faker->word,
         'name_on_account' => $faker->name,
-        'routing_number' => $this->faker->randomNumber(9),
+        'routing_number' => $this->faker->randomNumber(9, true),
         'account_number' => $faker->bankAccountNumber,
         'account_type' => collect('checking', 'savings')->random(),
         'account_holder_type' => collect('business', 'personal')->random(),
