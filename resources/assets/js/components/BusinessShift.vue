a5fb3505459983242a8a25d581840276acbf48dd
faze <jonwillard11@gmail.com>
2019-06-25 09:12:54 -0400

    Fix repeating decimal issue on edit shift warning ALLY-1293

--- resources/assets/js/components/BusinessShift.vue
+++ resources/assets/js/components/BusinessShift.vue
@@ -344,7 +344,7 @@
                             </div>
 
                             <div v-if="billingType === 'services' && serviceHours != duration" class="alert alert-warning">
-                                Warning: The shift's actual hours ({{ duration }}) do not match the broken out service hours.
+                                Warning: The shift's actual hours ({{ numberFormat(duration) }}) do not match the broken out service hours.
                             </div>
                             <b-alert v-if="isUsingOvertime" variant="warning" show>
                                 Note: Because OT/HOL is selected, the rates have been re-calculated to match your settings.
