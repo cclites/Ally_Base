d5a268ae5f48db65ac7239d00df50e88b4829468
Chad Clites <chad@extant.digital>
2019-06-25 20:57:46 -0500

    a little work needed on the query yet

--- app/Http/Controllers/Business/ExpirationsFilterController.php
+++ app/Http/Controllers/Business/ExpirationsFilterController.php
@@ -23,49 +23,24 @@ class ExpirationsFilterController extends Controller
 
     public function index(Request $request){
 
-        /*
+
         Log::info(json_encode($request->all()));
 
-        if($request->caregiver_id){
-            $caregiverIds[] = $request->caregiver_id;
-        }else{
-
-            $caregiverIds = Caregiver::forRequestedBusinesses([$request->business_id], null)->query();
-
-
-
-
-        }
-
-        $caregiverIds = Caregiver::forRequestedBusinesses([$request->business_id], null)->select();
-
-        if(!$request->active){
-            $caregiverIds->where('caregiver_active', $request->active);
-        }
-
-        if($request->caregiver_id){
-
-        }else{
-            $caregiverIds->pluck('id')->toArray();
-        }
-
-        Log::info($caregiverIds);
-        */
-
-        /*
+        $caregiverIds = Caregiver::forRequestedBusinesses([$request->business_id], null)
+                        ->pluck('id')->toArray();
 
         $report = new CertificationExpirationReport();
         $report->forBusinesses([$request->business_id])->orderBy('expires_at');
         $report->query()->whereIn('caregiver_id', $caregiverIds);
 
-
+        /*
         if($request->show_expired == 'false'){
             $today = Carbon::today();
-            $report->query()->where('expires_at', '<=', $today);
-        }
+            $report->query()->where('expires_at', '>=', $today);
+        }*/
 
         $expiresAt = Carbon::today()->addDays($request->days_range);
-        $report->query()->where('expires_at', '<=', $expiresAt);
+        $report->query()->where('expires_at', '>=', $expiresAt);
 
         if($request->name){
             $report->query()->where('name', $request->name);
@@ -73,56 +48,13 @@ class ExpirationsFilterController extends Controller
 
         $certifications = $report->rows();
 
-        Log::info(json_encode($certifications));
+        if($request->active){
+            $certifications = $certifications->where('caregiver_active', $request->active)->get();
+        }
+
+        Log::info($certifications);
 
         return response()->json($certifications);
 
-        /*
-        $this->query = CaregiverLicense::whereHas('caregiver', function($q){
-            where('')
-        });
-
-        //return response()->json($request->all());
-
-        $this->forCaregiverActive($request->active);
-        $this->forExpirationType($request->name);
-        $this->forLicensesExpiring($request->days_range);
-        $this->forExpired($request->show_expired);
-
-        return response()->json($this->query->get());
-        */
-    }
-
-    public function forBusiness($businessId){
-        Log::info("Business ID = $businessId");
-
-        $this->query->where('business_id', $businessId);
-    }
-
-    public function forCaregiver($caregiverId){
-
-        if($caregiverId){
-            $this->query->where('id', $caregiverId);
-        }
-
-        Log::info("Caregiver = $caregiverId");
-    }
-
-    public function forCaregiverActive($active){
-        Log::info("Active = $active");
-    }
-
-    public function forExpirationType($name){
-        Log::info("Name = $name");
-    }
-
-    public function forLicensesExpiring($range){
-        Log::info("Range = $range");
-    }
-
-    public function forExpired($expired){
-        Log::info("Expired = $expired");
     }
 }
-
-//"caregiver_id":null,"days_range":"30","show_expired":"false","active":null,"name":null,"business_id":"57"}
