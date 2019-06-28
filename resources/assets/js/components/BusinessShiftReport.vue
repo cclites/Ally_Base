256063555470b6f1468fb2c7542b0e482874d7ff
faze <jonwillard11@gmail.com>
2019-06-28 11:09:09 -0400

    Remove dead code and old filters modal from SHR

--- resources/assets/js/components/BusinessShiftReport.vue
+++ resources/assets/js/components/BusinessShiftReport.vue
@@ -110,12 +110,7 @@
                     </b-row>
                 </b-container>
 
-                <!--<b-col lg="8">-->
-                    <!--<small v-show="loaded >= 3" v-html="filterDescription"></small>-->
-                <!--</b-col>-->
-
                 <b-col lg="12" class="text-right">
-                    <!--<b-button type="button" @click="filtersModal = true" variant="info" class="ml-2">Update Filters and Generate Report</b-button>-->
                     <b-btn variant="info" @click="reloadData()">Generate Report</b-btn>
                     <b-button type="button" @click="showHideSummary()" variant="primary" class="ml-2" v-show="shiftsLoaded">{{ summaryButtonText }}</b-button>
                 </b-col>
@@ -167,117 +162,10 @@
                                 <b-btn size="sm" @click.stop="confirmShift(row.item)" variant="primary" v-b-tooltip.hover title="Confirm" v-else-if="row.item.status !== 'Clocked In'"><i class="fa fa-calendar-check-o"></i></b-btn>
                             </span>
                         <b-btn size="sm" @click.stop="deleteShift(row.item)" variant="danger" v-b-tooltip.hover title="Delete"><i class="fa fa-times"></i></b-btn>
-
-                        <!--<b-dropdown split variant="light" text="Edit" class="m-2" @click="editingShiftId = row.item.id; editShiftModal = true">-->
-                        <!--<b-dropdown-item @click.stop="details(row.item)">View Details</b-dropdown-item>-->
-                        <!--<b-dropdown-item @click.stop="unconfirmShift(row.item)" v-if="row.item.Confirmed">Unconfirm Shift</b-dropdown-item>-->
-                        <!--<b-dropdown-item @click.stop="confirmShift(row.item)" v-else-if="row.item.status !== 'Clocked In'">Confirm Shift</b-dropdown-item>-->
-                        <!--<b-dropdown-divider></b-dropdown-divider>-->
-                        <!--<b-dropdown-item @click="deleteShift(row.item)"><i class="fa fa-times"></i> Delete</b-dropdown-item>-->
-                        <!--</b-dropdown>-->
                     </div>
                 </template>
             </shift-history-table>
         </b-card>
-
-        <b-modal size="lg" id="filtersModal" title="Select Date Range &amp; Filters">
-            <!-- This modal is temporarily hidden, add v-model="filtersModal" to re-enable -->
-            <!-- The filters are available inline instead -->
-            <b-container fluid>
-                <b-row>
-                    <b-col lg="6">
-                        <b-form-group label="Date Range" class="form-inline">
-                            <date-picker ref="startDate"
-                                         v-model="filters.start_date"
-                                         placeholder="Start Date">
-                            </date-picker> &nbsp;to&nbsp;
-                            <date-picker ref="endDate"
-                                         v-model="filters.end_date"
-                                         placeholder="End Date">
-                            </date-picker>
-                        </b-form-group>
-                        <b-form-group label="Caregiver" class="form-inline">
-                            <b-form-select v-model="filters.caregiver_id" ref="caregiverFilter">
-                                <option value="">All Caregivers</option>
-                                <option v-for="item in caregivers" :value="item.id" :key="item.id">{{ item.nameLastFirst }}</option>
-                            </b-form-select>
-                        </b-form-group>
-                        <b-form-group label="Client" class="form-inline">
-                            <b-form-select v-model="filters.client_id" ref="clientFilter">
-                                <option value="">All Clients</option>
-                                <option v-for="item in clients" :value="item.id" :key="item.id">{{ item.nameLastFirst }}</option>
-                            </b-form-select>
-                        </b-form-group>
-                        <!-- Business Location is not shown on single business registries -->
-                        <business-location-form-group class="form-inline" v-model="filters.business_id" :allow-all="true" />
-                    </b-col>
-                    <b-col lg="6">
-                        <b-form-group label="Payment Method" class="form-inline">
-                            <b-form-select v-model="filters.payment_method" ref="paymentFilter">
-                                <option value="">All Payment Methods</option>
-                                <option value="credit_card">Credit Card</option>
-                                <option value="bank_account">Bank Account</option>
-                                <option value="business">Provider Payment</option>
-                            </b-form-select>
-                        </b-form-group>
-                        <b-form-group label="Charge Status" class="form-inline">
-                            <b-form-select v-model="filters.charge_status" ref="chargeFilter">
-                                <option value="">All Statuses</option>
-                                <option value="charged">Charged</option>
-                                <option value="uncharged">Un-Charged</option>
-                            </b-form-select>
-                        </b-form-group>
-                        <b-form-group label="Confirmed Status" class="form-inline">
-                            <b-form-select v-model="filters.confirmed_status" ref="confirmedFilter">
-                                <option value="">All Statuses</option>
-                                <option value="confirmed">Confirmed</option>
-                                <option value="unconfirmed">Unconfirmed</option>
-                            </b-form-select>
-                        </b-form-group>
-                        <!-- ADMIN ONLY DROPDOWN -->
-                        <b-form-group label="Admin Imports" class="form-inline" v-if="admin">
-                            <b-form-select v-model="filters.import_id">
-                                <option value="">--Filter by Import--</option>
-                                <option v-for="item in imports" :value="item.id" :key="item.id">{{ item.name }} ({{ item.created_at }})</option>
-                            </b-form-select>
-                        </b-form-group>
-                    </b-col>
-                </b-row>
-                <b-row>
-                    <b-col lg="12">
-                        <div class="card">
-                            <div class="card-body">
-                                <h6 class="card-title">Filter by Flags</h6>
-                                <b-form-radio-group v-model="filters.flag_type" @change="updateFilterFlags">
-                                    <b-radio value="any">Include All Shifts - Flagged or Not</b-radio><br />
-                                    <b-radio value="none">Has No Flags</b-radio><br />
-                                    <b-radio value="selected">Has Any of the Selected Flags:</b-radio>
-                                </b-form-radio-group>
-                                <b-col lg="12">
-                                    <b-form-checkbox v-model="includeAllFlags" @change="updateFilterFlags" :disabled="filters.flag_type !== 'selected'">All Flags</b-form-checkbox>
-                                    <b-form-checkbox v-for="(display, value) in flagTypes"
-                                                     v-model="filters.flags"
-                                                     :value="value"
-                                                     :key="value"
-                                                     class="flag-checkbox"
-                                                     :disabled="filters.flag_type !== 'selected'"
-                                                     @change="includeAllFlags = false"
-                                    >
-                                        {{ display }}
-                                    </b-form-checkbox>
-                                </b-col>
-                            </div>
-                        </div>
-                    </b-col>
-                </b-row>
-            </b-container>
-            <div slot="modal-footer">
-                <b-btn variant="info" @click="reloadData()">
-                    Generate Report
-                </b-btn>
-                <b-btn variant="default" @click="filtersModal=false">Close</b-btn>
-            </div>
-        </b-modal>
 
         <!-- Filter columns modal -->
         <filter-columns-modal v-model="columnsModal"
@@ -297,8 +185,8 @@
             </template>
         </shift-details-modal>
 
-        <add-shift-modal 
-            v-model="addShiftModal" 
+        <add-shift-modal
+            v-model="addShiftModal"
             :caregiver="{id: filters.caregiver_id}"
             :client="{id: filters.client_id}"
             @shift-created="onShiftCreate"
@@ -381,7 +269,6 @@
                 showSummary: false,
                 sortBy: 'Day',
                 sortDesc: false,
-                filtersModal: !this.autoload,
                 addShiftModal: false,
                 editShiftModal: false,
                 detailsModal: false,
@@ -550,7 +437,7 @@
                 }
                 return (item.confirmed) ? null : 'warning';
             },
-            
+
             fullscreenToggle() {
                 $(this.$refs.SHRCard).toggleClass('fullscreen-shr');
                 $('.left-sidebar').toggle();
@@ -619,7 +506,6 @@
             },
 
             loadData() {
-                this.filtersModal = false;
                 this.filterDescription = this.getFilterDescription();
                 this.loadingShifts = true;
 
@@ -636,7 +522,6 @@
                         if (error.response.data && error.response.data.message) {
                             alerts.addMessage('error', error.response.data.message);
                         }
-                        this.filtersModal = true;
                     })
                     .finally(() => {
                         this.shiftsLoaded = true;
@@ -889,9 +774,6 @@
     }
     .shift-table td > .fa {
         font-size: 16px;
-    }
-    #filtersModal .datepicker, #filtersContainer .datepicker {
-        max-width: 150px;
     }
     #filtersContainer .form-group {
         margin-bottom: 0.5rem;
