368758e70b57bd93b44d26f6968c282386f2f18e
Chad Clites <chad@extant.digital>
2019-07-10 09:43:58 -0500

    wip

---
+++ app/Http/Controllers/Business/Report/ThirdPartyPayerReportController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Business\Report;
+
+use App\Caregiver;
+use App\Client;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CaregiverDropdownResource;
+use App\Http\Resources\ClientDropdown;
+use App\Http\Resources\ClientDropdownResource;
+use App\Reports\MedicaidBillingReport;
+use App\Reports\ThirdPartyPayerReport;
+use Illuminate\Http\Request;
+
+class ThirdPartyPayerReportController extends Controller
+{
+    /**
+     * Get the Payroll Export Report
+     *
+     * @param Request $request
+     * @param MedicaidBillingReport $report
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+    public function index(Request $request, ThirdPartyPayerReport $report)
+    {
+        if ($request->filled('json')) {
+            $report = $report->forDates($request->start, $request->end, auth()->user()->role->getTimezone())
+                ->forClientType($request->client_type)
+                ->forClient($request->client)
+                ->forCaregiver($request->caregiver);
+
+            return response()->json($report->rows());
+        }
+
+        $clients = new ClientDropdownResource(Client::forRequestedBusinesses()->active()->get());
+        $caregivers = new CaregiverDropdownResource(Caregiver::forRequestedBusinesses()->active()->get());
+
+        return view_component(
+            'business-medicaid-billing-report',
+            'Medicaid Billing Report',
+            compact(['clients', 'caregivers']),
+            [
+            'Home' => route('home'),
+            'Reports' => route('business.reports.index')
+            ]
+        );
+    }
+}
