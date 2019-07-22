cd9a57ec53df13d6c6ce0610276fc19ac5af68a8
faze <jonwillard11@gmail.com>
2019-07-22 13:04:00 -0500

    typo

--- resources/assets/js/components/business/reports/ThirdPartyPayerReport.vue
+++ resources/assets/js/components/business/reports/ThirdPartyPayerReport.vue
@@ -92,7 +92,7 @@
                             </template>
                             <template slot="actions" scope="row">
                                 <a :href="`/business/claims-ar?start_date=${form.start}&end_date=${form.end}&filter=${row.item.invoice_name}`" target="_blank">
-                                    <b-btn variant="secondary">Goto Claims</b-btn>
+                                    <b-btn variant="secondary">Go to Claims</b-btn>
                                 </a>
                             </template>
                         </b-table>
