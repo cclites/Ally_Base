f092e732182b99bebadc0891688aed3b0b3e6204
faze <jonwillard11@gmail.com>
2019-07-29 16:48:30 -0400

    Add twilio sandbox mode to sms blasting ALLY-1391

--- app/Http/Requests/SendTextRequest.php
+++ app/Http/Requests/SendTextRequest.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Http\Requests;
 
+use App\Caregiver;
+use App\PhoneNumber;
+use Illuminate\Support\Collection;
+
 class SendTextRequest extends BusinessRequest
 {
     /**
@@ -16,5 +20,50 @@ class SendTextRequest extends BusinessRequest
             'recipients.*' => 'integer',
             'can_reply' => 'boolean',
         ];
+    }
+
+    /**
+     * Scrub the requested list of recipients to those that are
+     * for the requested business, are active and have a SMS-enabled
+     * phone number stored.  If no recipient IDs are present in the
+     * request, all eligible Caregivers will be returned.
+     *
+     * @return Collection
+     */
+    public function getEligibleCaregivers() : Collection
+    {
+        $query = Caregiver::with('phoneNumbers')
+            ->forRequestedBusinesses()
+            ->active()
+            ->whereHas('phoneNumbers', function ($q) {
+                $q->where('receives_sms', 1);
+            });
+
+        if (filled($this->input('recipients'))) {
+            $query->whereIn('id', $this->input('recipients'));
+        }
+
+        return $query->get()->toBase();
+    }
+
+    /**
+     * Get the outgoing SMS number for the current business.
+     * Returns default twilio number if no number set and
+     * Returns null if no number set and replies are turned on.
+     *
+     * @return string|null
+     */
+    public function getOutgoingNumber() : ?string
+    {
+        if ($from = $this->getBusiness()->outgoing_sms_number) {
+            return $from;
+        }
+
+        if ($this->input('can_reply')) {
+            // Cannot proceed
+            return null;
+        }
+
+        return PhoneNumber::formatNational(config('services.twilio.default_number'));
     }
 }
