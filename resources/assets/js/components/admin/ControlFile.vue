f495356b55cd60c4ed7b6fa6e03637a19ee47371
1337erik <erikpwhite@gmail.com>
2019-07-31 12:57:07 -0400

    adjusted form to fit our needs, driving the form display and data with an array

--- resources/assets/js/components/admin/ControlFile.vue
+++ resources/assets/js/components/admin/ControlFile.vue
@@ -1,56 +1,51 @@
 <template>
 
-  <div>
+    <b-row>
 
-    <b-form @submit=" onSubmit " @reset=" onReset " v-if=" show ">
+        <b-col cols="12">
 
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
+            <b-form @submit=" onSubmit " @reset=" onReset " v-if=" show ">
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Enter name"
-        ></b-form-input>
-      </b-form-group>
+                <b-col
+                    v-for=" ( field, index ) in formModel "
+                    :key=" index "
+                    :cols=" field.col_xs "
+                    :sm=" field.col_sm "
+                    :md=" field.col_md "
+                    :lg=" field.col_lg "
+                >
 
-      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-        <b-form-select
-          id="input-3"
-          v-model="form.food"
-          :options="foods"
-          required
-        ></b-form-select>
-      </b-form-group>
+                    <b-form-group
+                        :id=" 'input-group-' + index "
+                        :label=" field.fieldTitle + ':' "
+                        :label-for=" 'input-' + index "
+                        :description=" field.description "
+                    >
 
-      <b-form-group id="input-group-4">
-        <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-          <b-form-checkbox value="me">Check me out</b-form-checkbox>
-          <b-form-checkbox value="that">Check that out</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
+                        <b-form-input
+                            :id=" 'input-' + index "
+                            v-model=" form[ field.fieldName ] "
+                            :type=" field.fieldType "
+                            :required=" field.required "
+                            :placeholder=" field.placeholder "
+                            :size=" field.length "
+                        ></b-form-input>
+                    </b-form-group>
+                </b-col>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-    <b-card class="mt-3" header="Form Data Result">
-      <pre class="m-0">{{ form }}</pre>
-    </b-card>
-  </div>
+                <b-button type="submit" variant="primary">Submit</b-button>
+                <b-button type="reset" variant="danger">Reset</b-button>
+            </b-form>
+        </b-col>
+
+        <b-col cols="12">
+
+            <b-card class="mt-3" header="Form Data Result">
+
+                <pre class="m-0">{{ form }}</pre>
+            </b-card>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
@@ -60,16 +55,181 @@
 
             return {
 
+                show      : true, // a bootstrap-vue thing, trick to reset the form
                 form      : {},
-                formModel : {
+                formModel : [
 
-                    email   : '',
-                    name    : '',
-                    food    : null,
-                    checked : []
-                },
-                foods: [ { text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn' ],
-                show : true
+                    {
+                        fieldTitle  : 'Control Total Indicator',
+                        fieldName   : 'cti',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 2,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Customer Point',
+                        fieldName   : 'cp',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 8,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Customer CA',
+                        fieldName   : 'cc',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 9,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Customer CA Type',
+                        fieldName   : 'cctype',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 6,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Entry/Addenda Count Indicator',
+                        fieldName   : 'caci',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 1,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Entry/Addenda Count',
+                        fieldName   : 'cac',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 8,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Debit Amount',
+                        fieldName   : 'da',
+                        fieldType   : 'number',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 11,
+                        decimal     : 2,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Credit Amount',
+                        fieldName   : 'da',
+                        fieldType   : 'number',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 11,
+                        decimal     : 2,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Hash',
+                        fieldName   : 'hash',
+                        fieldType   : 'number',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 9,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                    {
+                        fieldTitle  : 'Filler',
+                        fieldName   : 'filler',
+                        fieldType   : 'text',
+                        baseValue   : '',
+                        placeholder : 'Enter Value',
+                        description : '',
+                        length      : 24,
+                        decimal     : 0,
+                        fillChar    : ' ',
+                        justify     : 'right',
+                        required    : true,
+                        col_xs      : 12,
+                        col_sm      : 6,
+                        col_md      : 4,
+                        col_lg      : 3
+                    },
+                ],
             }
         },
         methods: {
@@ -95,7 +255,11 @@
             },
             setForm(){
 
-                this.form = _.cloneDeep( this.formModel );
+                this.form = _.reduce( this.formModel, ( obj, param ) => {
+
+                    obj[ param.fieldName ] = param.baseValue;
+                    return obj;
+                }, {} );
             }
         },
         mounted(){
