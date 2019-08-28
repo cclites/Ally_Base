1b9d4cde2510dd5b288f99acdcbc5dc788eb538f
Chad Clites <chad@extant.digital>
2019-08-28 09:42:44 -0500

    removed Payer Report Controller

--- app/Http/Controllers/Business/Report/PaymentSummaryByPayerReportController.php
+++ app/Http/Controllers/Business/Report/PaymentSummaryByPayerReportController.php
@@ -69,7 +69,7 @@ class PaymentSummaryByPayerReportController extends BaseController
      * @param $data
      * @return array
      */
-    protected function createSummary(): array
+    protected function createSummary($data): array
     {
         $set = [];
 
