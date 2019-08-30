ba6414f91bec2569c3068a36fdcb0adf841ac50c
faze <jonwillard11@gmail.com>
2019-08-30 13:54:41 -0400

    Revert changes to client invoice item factory
    Issue with failing tests

--- database/factories/ClientInvoiceItemFactory.php
+++ database/factories/ClientInvoiceItemFactory.php
@@ -1,12 +1,9 @@
 <?php
 
-use App\Billing\ClientInvoice;
-use App\Shift;
 use Faker\Generator as Faker;
 
 $factory->define(\App\Billing\ClientInvoiceItem::class, function (Faker $faker) {
     return [
-
         'name' => $faker->sentence(3),
         'units' => $units = mt_rand(1,4),
         'rate' => $rate = $faker->randomFloat(2, 10, 20),
@@ -14,8 +11,5 @@ $factory->define(\App\Billing\ClientInvoiceItem::class, function (Faker $faker) 
         'amount_due' => $units * $rate * $faker->randomFloat(1, 0, 1),
         'date' => $faker->dateTime,
         'notes' => $faker->randomElement([null, $faker->sentence]),
-        'invoice_id'       => factory( ClientInvoice::class ),
-        'invoiceable_type' => 'shifts',
-        'invoiceable_id'   => factory( Shift::class ),
     ];
 });
