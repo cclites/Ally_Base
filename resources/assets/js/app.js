43bc1ecdb01bed006b981b89927fb361690a6995
faze <jonwillard11@gmail.com>
2019-09-18 15:11:44 -0400

    Fix bug with invalid vue component name

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -71,6 +71,7 @@ Vue.component('confirm-modal', require('./components/modals/ConfirmModal'));
 Vue.component('client-contacts-tab', require('./components/ClientContactsTab'));
 Vue.component('client-contacts-modal', require('./components/ClientContactsModal'));
 Vue.component('shift-details', require('./components/shifts/ShiftDetails'));
+Vue.component('payer-dropdown-filter', require('./components/filters/PayerDropdownFilter');
 
 // Client
 Vue.component('ltc-shift-approval', require('./components/clients/LtcShiftApproval'));
@@ -390,8 +391,6 @@ Vue.component('claim-remits', require('./components/business/claims/ClaimRemits'
 Vue.component('apply-remit-page', require('./components/business/claims/ApplyRemit'));
 Vue.component('business-claim-invoice-aging-report', require('./components/business/claims/ClaimInvoiceAgingReport'));
 Vue.component('remit-application-history', require('./components/business/claims/RemitApplicationHistory'));
-
-Vue.component('payer-dropdown-filter', require('./components/filters/PayerDropdownFilter'));
 
 Vue.filter('date', value => {
     return moment.utc(value).local().format('L');
