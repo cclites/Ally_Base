a8c6427b69b000b81d9d95ec24effd85b647a7a1
Chad Clites <chad@extant.digital>
2019-10-07 12:23:58 -0500

    pause for clarification of specifications

--- resources/assets/js/components/business/EmailTemplates.vue
+++ resources/assets/js/components/business/EmailTemplates.vue
@@ -6,6 +6,13 @@
         <b-select v-model="selectedType">
             <option v-for="type in types" :key="type.id">{{ type.name }}</option>
         </b-select>
+
+        <b-row class="pane caregiver_expirations">
+            <div>
+                <b-input v-model="cg_expire.greeting"></b-input>
+            </div>
+        </b-row>
+
     </b-card>
 </template>
 
@@ -19,11 +26,21 @@
         data() {
             return {
                 selectedType: "",
+                cg_expire: {
+                    greeting: "",
+                }
             };
         },
+        watch: {
+            selectedType(){
+
+            },
+        }
     }
 </script>
 
 <style scoped>
-
+    .pane{
+        display: none;
+    }
 </style>
