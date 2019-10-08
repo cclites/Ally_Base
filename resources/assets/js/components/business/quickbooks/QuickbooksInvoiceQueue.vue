1e0cda0e8ad28105bca8a4086db83f3295e1478a
faze <jonwillard11@gmail.com>
2019-10-08 17:28:27 -0400

    wip

--- resources/assets/js/components/business/quickbooks/QuickbooksInvoiceQueue.vue
+++ resources/assets/js/components/business/quickbooks/QuickbooksInvoiceQueue.vue
@@ -1,94 +1,128 @@
 <template>
-    <b-card>
+    <div>
         <b-row>
-            <b-col lg="12">
-                <b-card header="Select Date Range"
-                        header-text-variant="white"
-                        header-bg-variant="info"
-                >
-                    <b-form inline @submit.prevent="loadItems()">
-                        <date-picker
+            <b-col>
+                <business-location-form-group
+                    v-model="business_id"
+                    label="Choose Office Location"
+                />
+            </b-col>
+        </b-row>
+        <b-card header="Quickbooks Invoice Queue"
+        header-text-variant="white"
+        header-bg-variant="info"
+    >
+        <b-row v-if="loadingConnection">
+            <b-col>
+                <loading-card />
+            </b-col>
+        </b-row>
+        <div v-else> <!-- Not initializing -->
+            <b-row v-if="! hasConnectionConfigured">
+                <b-col>
+                    <b-alert show variant="danger">
+                        You must configure your <a href="/business/quickbooks">Quickbooks Connection</a> for this office location before you can transfer invoices.
+                    </b-alert>
+                </b-col>
+            </b-row>
+            <div v-else> <!-- Connection is configured -->
+                <b-row>
+                    <b-col>
+                        <b-alert show variant="info">This search does not include Offline Client invoices.</b-alert>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="mb-4">
+                        <b-form inline @submit.prevent="fetch()">
+                            <date-picker
                                 v-model="start_date"
                                 placeholder="Start Date"
                                 class="mt-1"
-                        >
-                        </date-picker> &nbsp;to&nbsp;
-                        <date-picker
+                            >
+                            </date-picker> &nbsp;to&nbsp;
+                            <date-picker
                                 v-model="end_date"
                                 placeholder="End Date"
                                 class="mr-1 mt-1"
-                        >
-                        </date-picker>
-                        <b-form-select v-model="clientFilter" class="mr-1 mt-1">
-                            <option v-if="loadingClients" selected>Loading...</option>
-                            <option v-else value="">-- Select a Client --</option>
-                            <option v-for="item in clients" :key="item.id" :value="item.id">{{ item.nameLastFirst }}</option>
-                        </b-form-select>
-                        <b-form-select v-model="payerFilter" class="mr-1 mt-1">
-                            <option v-if="loadingPayers" selected>Loading...</option>
-                            <option v-else value="">-- Select a Payer --</option>
-                            <option value="0">(Client)</option>
-                            <option v-for="item in payers" :key="item.id" :value="item.id">{{ item.name }}</option>
-                        </b-form-select>
-                        &nbsp;<br /><b-button type="submit" variant="info" class="mt-1" :disabled="loaded === 0">Generate Report</b-button>
-                    </b-form>
-                </b-card>
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col lg="12" class="text-right">
-                <b-form-input v-model="filter" placeholder="Type to Search" />
-            </b-col>
-        </b-row>
-        <loading-card v-if="loaded == 0"></loading-card>
-        <b-row v-if="loaded < 0">
-            <b-col lg="12">
-                <b-card class="text-center text-muted">
-                    Select filters and press Generate Report
-                </b-card>
-            </b-col>
-        </b-row>
-        <div class="table-responsive" v-if="loaded > 0">
-            <b-table bordered striped hover show-empty
-                     :items="filteredItems"
-                     :fields="fields"
-                     :sort-by.sync="sortBy"
-                     :sort-desc.sync="sortDesc"
-                     :filter="filter"
-            >
-                <template slot="name" scope="row">
-                    <a :href="invoiceUrl(row.item)" target="_blank">{{ row.value }}</a>
-                </template>
-                <template slot="client" scope="row">
-                    <a :href="`/business/clients/${row.item.client.id}`">{{ row.item.client.name }}</a>
-                </template>
-                <template slot="actions" scope="row">
-                    <div v-if="row.item.quickbooksInvoice && row.item.quickbooksInvoice.length">
-                        Transferred on {{ formatDateFromUTC(row.item.quickbooksInvoice[0].created_at) }}
-                    </div>
-                    <b-btn v-else variant="primary" class="mr-2" @click="transfer(row.item)" :disabled="busy">
-                        <i v-if="row.item.id === transferringId" class="fa fa-spin fa-spinner"></i>
-                        <span>Transfer to Quickbooks</span>
-                    </b-btn>
-                </template>
-            </b-table>
+                            >
+                            </date-picker>
+                            <b-form-select v-model="clientFilter" class="mr-1 mt-1">
+                                <option v-if="loadingClients" selected>Loading...</option>
+                                <option v-else value="">-- Select a Client --</option>
+                                <option v-for="item in clients" :key="item.id" :value="item.id">{{ item.nameLastFirst }}</option>
+                            </b-form-select>
+
+                            <payer-dropdown v-model="payerFilter" class="mr-1 mt-1" empty-text="-- Select a Payer --" />
+
+                            &nbsp;<br /><b-button type="submit" variant="info" class="mt-1" :disabled="loadingTable">Generate</b-button>
+                        </b-form>
+                    </b-col>
+                </b-row>
+                <b-row v-if="waitingToGenerate">
+                    <b-col>
+                        <b-card class="text-center text-muted">
+                            Select filters and press Generate
+                        </b-card>
+                    </b-col>
+                </b-row>
+                <b-row v-else>
+                    <b-col>
+                        <b-form-input v-model="filter" placeholder="Type to Search" class="mb-2" />
+
+                        <div class="table-responsive">
+                            <b-table bordered striped hover show-empty
+                                :items="items"
+                                :fields="fields"
+                                :sort-by.sync="sortBy"
+                                :sort-desc.sync="sortDesc"
+                                :filter="filter"
+                                :busy="loadingTable"
+                            >
+                                <template slot="name" scope="row">
+                                    <a :href="invoiceUrl(row.item)" target="_blank">{{ row.value }}</a>
+                                </template>
+                                <template slot="client" scope="row">
+                                    <a :href="`/business/clients/${row.item.client.id}`">{{ row.item.client.name }}</a>
+                                </template>
+                                <template slot="actions" scope="row">
+                                    <div v-if="row.item.quickbooksInvoice && row.item.quickbooksInvoice.length">
+                                        Transferred on {{ formatDateFromUTC(row.item.quickbooksInvoice[0].created_at) }}
+                                    </div>
+                                    <b-btn v-else variant="primary" class="mr-2" @click="transfer(row.item)" :disabled="busy">
+                                        <i v-if="row.item.id === transferringId" class="fa fa-spin fa-spinner"></i>
+                                        <span>Transfer to Quickbooks</span>
+                                    </b-btn>
+                                </template>
+                            </b-table>
+                        </div>
+                    </b-col>
+                </b-row>
+            </div>
         </div>
     </b-card>
+    </div>
 </template>
 
 <script>
+    import BusinessLocationFormGroup from "../BusinessLocationFormGroup";
     import FormatsDates from "../../../mixins/FormatsDates";
     import FormatsNumbers from "../../../mixins/FormatsNumbers";
 
     export default {
         mixins: [FormatsDates, FormatsNumbers],
+        components: { BusinessLocationFormGroup },
 
         data() {
             return {
+                loadingConnection: true,
+                loadingTable: false,
+                waitingToGenerate: true,
+
+                connection: {},
+                business_id: null,
                 sortBy: 'shift_time',
                 sortDesc: false,
                 filter: null,
-                loaded: -1,
                 start_date: moment().subtract(30, 'days').format('MM/DD/YYYY'),
                 end_date: moment().format('MM/DD/YYYY'),
                 invoiceType: "",
@@ -145,15 +179,21 @@
         },
 
         mounted() {
-            this.loadClients();
-            this.fetchPayers();
+            if (this.business_id) {
+                this.fetchConnection();
+            }
         },
 
         computed: {
-            filteredItems() {
-                return this.items.map(item => {
-                    return item;
-                });
+            mode() {
+                if (! this.hasConnectionConfigured) {
+                    return null;
+                }
+                return this.connection.is_desktop ? 'offline' : 'online';
+            },
+
+            hasConnectionConfigured() {
+                return this.connection && this.connection.is_authenticated;
             },
         },
 
@@ -177,18 +217,6 @@
                     });
             },
 
-            async fetchPayers() {
-                this.payers = [];
-                this.loadingPayers = true;
-                let response = await axios.get('/business/payers?json=1');
-                if (Array.isArray(response.data)) {
-                    this.payers = response.data;
-                } else {
-                    this.payers = [];
-                }
-                this.loadingPayers = false;
-            },
-
             async loadClients() {
                 this.clients = [];
                 this.loadingClients = true;
@@ -197,8 +225,9 @@
                 this.loadingClients = false;
             },
 
-            async loadItems() {
-                this.loaded = 0;
+            async fetch() {
+                this.waitingToGenerate = false;
+                this.loadingTable = true;
                 let url = `/business/quickbooks-queue?json=1&start_date=${this.start_date}&end_date=${this.end_date}&invoiceType=${this.invoiceType}&client_id=${this.clientFilter}&payer_id=${this.payerFilter}`;
                 axios.get(url)
                     .then( ({ data }) => {
@@ -208,14 +237,39 @@
                         this.items = [];
                     })
                     .finally(() => {
-                        this.loaded = 1;
+                        this.loadingTable = false;
                     });
+            },
+
+            fetchConnection() {
+                this.loadingConnection = true;
+                axios.get(`/business/quickbooks?business_id=${this.business_id}&json=1`)
+                    .then( ({ data }) => {
+                        this.connection = data.connection;
+                        this.clients = data.clients;
+                    })
+                    .catch(() => {
+                        this.connect = {};
+                        this.clients = [];
+                    })
+                    .finally(() => {
+                        this.loadingConnection = false;
+                    })
             },
 
             invoiceUrl(invoice, view="") {
                 return `/business/client/invoices/${invoice.id}/${view}`;
             },
-        }
+        },
+
+        watch: {
+            business_id(newValue, oldValue) {
+                if (newValue) {
+                    this.loading = true;
+                    this.fetchConnection();
+                }
+            },
+        },
     }
 </script>
 
