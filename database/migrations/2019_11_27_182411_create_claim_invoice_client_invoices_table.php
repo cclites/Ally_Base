46f19fedd4a3880a9c05d22cc074d2f6229e3e1a
faze <jonwillard11@gmail.com>
2019-12-05 07:31:59 -0500

    Add unique constrant to client invoices id on claims pivot table ALLY-1707

--- database/migrations/2019_11_27_182411_create_claim_invoice_client_invoices_table.php
+++ database/migrations/2019_11_27_182411_create_claim_invoice_client_invoices_table.php
@@ -16,7 +16,7 @@ class CreateClaimInvoiceClientInvoicesTable extends Migration
         Schema::create('claim_invoice_client_invoice', function (Blueprint $table) {
             $table->primary(['claim_invoice_id', 'client_invoice_id'], 'claim_invoice_client_invoice_pk');
             $table->unsignedBigInteger('claim_invoice_id');
-            $table->unsignedInteger('client_invoice_id');
+            $table->unsignedInteger('client_invoice_id')->unique();
 
             $table->foreign('claim_invoice_id', 'claim_invoice_client_invoice_claim_fk')->references('id')->on('claim_invoices')->onUpdate('CASCADE')->onDelete('CASCADE');
             $table->foreign('client_invoice_id', 'claim_invoice_client_invoice_client_fk')->references('id')->on('client_invoices')->onUpdate('CASCADE')->onDelete('CASCADE');
