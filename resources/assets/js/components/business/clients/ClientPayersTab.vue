70055adada03b346f902401a43e9d22550ab4023
faze <jonwillard11@gmail.com>
2019-12-06 11:40:01 -0500

    Resize text fields on client payers tab table ALLY-1707

--- resources/assets/js/components/business/clients/ClientPayersTab.vue
+++ resources/assets/js/components/business/clients/ClientPayersTab.vue
@@ -27,7 +27,7 @@
                     </b-btn>
                 </template>
                 <template slot="payer_id" scope="row">
-                    <b-select v-model="row.item.payer_id" class="form-control-sm">
+                    <b-select v-model="row.item.payer_id" class="form-control-sm w-auto">
                         <option :value="0">({{ client.name }})</option>
                         <option v-for="payer in payerOptions" :value="payer.id" :key="payer.id">{{ payer.name }}</option>
                         <option :value="1">OFFLINE</option>
@@ -43,13 +43,13 @@
                     <mask-input v-model="row.item.effective_end" type="date" class="date-input form-control-sm"></mask-input>
                 </template>
                 <template slot="program_number" scope="row">
-                    <b-form-input v-model="row.item.program_number" type="text" class="date-input form-control-sm" />
+                    <b-form-input v-model="row.item.program_number" type="text" class="form-control-sm wider-text" />
                 </template>
                 <template slot="cirts_number" scope="row">
-                    <b-form-input v-model="row.item.cirts_number" type="text" class="date-input form-control-sm" />
+                    <b-form-input v-model="row.item.cirts_number" type="text" class="form-control-sm wider-text" />
                 </template>
                 <template slot="notes" scope="row">
-                    <b-form-input v-model="row.item.notes" type="text" class="date-input form-control-sm" />
+                    <b-form-input v-model="row.item.notes" type="text" class="form-control-sm wider-text" />
                 </template>
                 <template slot="payment_allocation" scope="row">
                     <b-select v-model="row.item.payment_allocation" class="form-control-sm" >
@@ -272,5 +272,5 @@
 </script>
 
 <style>
-
+.wider-text { min-width: 150px!important; }
 </style>
