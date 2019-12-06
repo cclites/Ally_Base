10457f434887a42dd0db35a15d8c6c3011e6a3ac
1337erik <erikpwhite@gmail.com>
2019-12-06 14:37:09 -0500

    setting up the initial screens

---
+++ resources/assets/js/components/business/reports/AveryLabels.vue
@@ -0,0 +1,40 @@
+<template>
+    <b-card title="Avery Label Report">
+        <loading-card v-show=" loading " />
+        <div v-show=" !loading" class="table-responsive">
+
+            hey lmao
+            <!-- <ally-table id="avery-labels" :columns=" fields " :items="items" sort-by="nameLastFirst">
+
+                <template slot="name" scope="data">
+                    <a :href="`/business/${type}s/${data.item.id}`">{{ data.item.name }}</a>
+                </template>
+            </ally-table> -->
+        </div>
+    </b-card>
+</template>
+
+<script>
+
+
+    export default {
+
+        mixins: [],
+
+        props: {
+
+        },
+
+        data() {
+
+            return {
+
+                loading: false
+            }
+        },
+    }
+</script>
+
+<style scoped>
+</style>
+
