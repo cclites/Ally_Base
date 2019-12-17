f28d0790fdbe801fb5e80d6b137b37da91fbb746
faze <jonwillard11@gmail.com>
2019-12-17 14:35:32 -0500

    Convert caregiver 1099 query to eloquent

--- app/Admin/Queries/Caregiver1099Query.php
+++ app/Admin/Queries/Caregiver1099Query.php
@@ -1,13 +1,10 @@
 <?php
 
-
 namespace App\Admin\Queries;
 
-use App\Admin\Queries\BaseQuery;
-use App\Caregiver1099;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Collection;
-
+use App\Admin\Queries\BaseQuery;
 
 class Caregiver1099Query
 {
@@ -27,100 +24,101 @@ class Caregiver1099Query
         return $this->generateQuery();
     }
 
-    function generateQuery(){
+    function generateQuery()
+    {
         \DB::statement('set session sql_mode=\'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION\';');
 
         // IMPORTANT NOTE
         // The 1099 query needs to stay consistent year to year, we need to use the client payment date as the basis for inclusion in the tax year.
 
-        $query = "SELECT c.id as client_id, 
-                    u1.firstname as client_fname, 
-                    u1.lastname as client_lname,
-                    u1.email as client_email,  
-                    c.client_type, 
-                    c.ssn as client_ssn, 
-                    c.caregiver_1099,
-                    a1.address1 as client_address1, 
-                    a1.address2 as client_address2,
-                    a1.city as client_city,
-                    a1.state as client_state,
-                    a1.zip as client_zip,
-                    b.id as business_id, 
-                    b.name as business_name,
-                    u2.id as caregiver_id, 
-                    u2.firstname as caregiver_fname, 
-                    u2.lastname as caregiver_lname,
-                    u2.email as caregiver_email,
-                    c2.ssn as caregiver_ssn,
-                    c2.uses_ein_number,
-                    a2.address1 as caregiver_address1, 
-                    a2.address2 as caregiver_address2,
-                    a2.city as caregiver_city,
-                    a2.state as caregiver_state,
-                    a2.zip as caregiver_zip,
-                    ct.id as caregiver_1099_id,
-                    ct.payment_total as caregiver_1099_amount,
-                    ct.business_id as caregiver_1099_location,
-                    sum(h.caregiver_shift) as payment_total
-                    FROM clients c
-                    INNER JOIN shifts s ON s.client_id = c.id
-                    INNER JOIN payments p ON s.payment_id = p.id
-                    INNER JOIN shift_cost_history h ON h.id = s.id
-                    INNER JOIN users u1 ON u1.id = s.client_id
-                    INNER JOIN users u2 ON u2.id = s.caregiver_id
-                    INNER JOIN caregivers c2 ON c2.id = u2.id
-                    INNER JOIN businesses b ON c.business_id = b.id
-                    LEFT JOIN addresses a1 ON a1.id = (SELECT id FROM addresses WHERE user_id = u1.id ORDER BY `type` LIMIT 1)
-                    LEFT JOIN addresses a2 ON a2.id = (SELECT id FROM addresses WHERE user_id = u2.id ORDER BY `type` LIMIT 1)
-                    LEFT JOIN caregiver_1099s ct on ct.client_id = c.id AND ct.caregiver_id = c2.id
-                    WHERE p.created_at BETWEEN '" . $this->filters['year']['value'] ."-01-01 00:00:00' AND '" . $this->filters['year']['value'] ."-12-31 23:59:59'
-                    AND c.business_id = " .  $this->filters['business_id']['value'] . " AND c.send_1099 = 'yes' ";
+        $query = \DB::table('clients as c')
+            ->selectRaw("c.id as client_id,
+                u1.firstname as client_fname,
+                u1.lastname as client_lname,
+                u1.email as client_email,
+                c.client_type,
+                c.ssn as client_ssn,
+                c.caregiver_1099,
+                a1.address1 as client_address1,
+                a1.address2 as client_address2,
+                a1.city as client_city,
+                a1.state as client_state,
+                a1.zip as client_zip,
+                b.id as business_id,
+                b.name as business_name,
+                u2.id as caregiver_id,
+                u2.firstname as caregiver_fname,
+                u2.lastname as caregiver_lname,
+                u2.email as caregiver_email,
+                c2.ssn as caregiver_ssn,
+                c2.uses_ein_number,
+                a2.address1 as caregiver_address1,
+                a2.address2 as caregiver_address2,
+                a2.city as caregiver_city,
+                a2.state as caregiver_state,
+                a2.zip as caregiver_zip,
+                ct.id as caregiver_1099_id,
+                ct.payment_total as caregiver_1099_amount,
+                ct.business_id as caregiver_1099_location,
+                SUM(h.caregiver_shift) as payment_total
+            ")
+            ->join('shifts as s', 's.client_id', '=', 'c.id')
+            ->join('payments as p', 's.payment_id', '=', 'p.id')
+            ->join('shift_cost_history as h', 's.id', '=', 'h.id')
+            ->join('users as u1', 's.client_id', '=', 'u1.id')
+            ->join('users as u2', 's.caregiver_id', '=', 'u2.id')
+            ->join('caregivers as c2', 'u2.id', '=', 'c2.id')
+            ->join('businesses as b', 'c.business_id', '=', 'b.id')
+            ->leftJoin('caregiver_1099s as ct', function ($join) {
+                $join->on('c.id', '=', 'ct.client_id')
+                    ->on('c2.id', '=', 'ct.caregiver_id');
+            })
+            ->joinSub(\DB::table('addresses'), 'a1', 'a1.user_id', '=', 'u1.id', 'left')
+            ->joinSub(\DB::table('addresses'), 'a2', 'a2.user_id', '=', 'u2.id', 'left')
+            ->whereBetween('p.created_at', [
+                Carbon::parse($this->filters['year']['value'] . '-01-01 00:00:00'),
+                Carbon::parse($this->filters['year']['value'] . '-12-31 23:59:59')
+            ])
+            ->where('c.business_id', $this->filters['business_id']['value'])
+            ->where('c.send_1099', 'yes');
 
-
-        if( array_key_exists('client_id', $this->filters) && filled($this->filters['client_id']['value'])){
-            $query .= " AND u1.id = " . $this->filters['client_id']['value'];
+        if (array_key_exists('client_id', $this->filters) && filled($this->filters['client_id']['value'])) {
+            $query->where('u1.id', '=', $this->filters['client_id']['value']);
         }
 
-
-        if(array_key_exists('caregiver_id', $this->filters) && filled($this->filters['caregiver_id']['value'])){
-            $query .= " AND c2.id =" .  $this->filters['caregiver_id']['value'];
+        if (array_key_exists('caregiver_id', $this->filters) && filled($this->filters['caregiver_id']['value'])) {
+            $query->where('c2.id', '=', $this->filters['caregiver_id']['value']);
         }
 
-        if( array_key_exists('caregiver_1099', $this->filters) && filled($this->filters['caregiver_1099']['value'])){
-            if( $this->filters['caregiver_1099']['value'] && $this->filters['caregiver_1099']['value'] !== 'no'){
-                $query .= " AND c.caregiver_1099 = '" . (string)$this->filters['caregiver_1099']['value'] . "' ";
-            }elseif ( $this->filters['caregiver_1099']['value'] && $this->filters['caregiver_1099']['value'] === 'no' ){
-                $query .= " AND c.caregiver_1099 is null ";
+        if (array_key_exists('caregiver_1099', $this->filters) && filled($this->filters['caregiver_1099']['value'])) {
+            if ($this->filters['caregiver_1099']['value'] && $this->filters['caregiver_1099']['value'] !== 'no') {
+                $query->where('c.caregiver_1099', '=', (string)$this->filters['caregiver_1099']['value']);
+            } elseif ($this->filters['caregiver_1099']['value'] && $this->filters['caregiver_1099']['value'] === 'no') {
+                $query->whereNull('c.caregiver_1099');
             }
         }
 
-        if( array_key_exists('transmitted', $this->filters) && filled($this->filters['transmitted']['value'])) {
+        if (array_key_exists('transmitted', $this->filters) && filled($this->filters['transmitted']['value'])) {
             if ($this->filters['transmitted']['value'] && $this->filters['transmitted']['value'] === 1) {
-                $query .= " AND ct.transmitted_at is not null ";
-            } elseif ($this->filters['transmitted']['value'] && $this->filters['transmitted']['value']) {
-                $query .= " AND ct.transmitted_at is null ";
+                $query->whereNotNull('ct.transmitted_at');
+            } elseif ($this->filters['transmitted']['value'] && $this->filters['transmitted']['value'] === 0) {
+                // TODO: check if this if statement is correct
+                $query->whereNull('ct.transmitted_at');
             }
         }
 
-        if( array_key_exists('created', $this->filters) && filled($this->filters['created']['value'])) {
+        if (array_key_exists('created', $this->filters) && filled($this->filters['created']['value'])) {
             if ($this->filters['created']['value'] && $this->filters['created']['value'] === 1) {
-                $query .= " AND ct.id is not null ";
+                $query->whereNotNull('ct.id');
             } elseif ($this->filters['created']['value'] && $this->filters['created']['value'] === 0) {
-                $query .= " AND ct.id is null ";
+                $query->whereNull('ct.id');
             }
         }
 
-        /*
-        if( array_key_exists('report_type', $this->filters) && filled($this->filters['report_type']['value'])){
-            $query .= " GROUP BY s.caregiver_id, s.business_id ";
-        }else{
-            $query .= " GROUP BY s.client_id, s.caregiver_id ";
-        }
-        */
-        $query .= " GROUP BY s.client_id, s.caregiver_id ";
-        $query .= "HAVING payment_total > ?";
+        $query->groupBy('s.client_id', 's.caregiver_id')
+            ->having('payment_total', '>=', $this->threshold);
 
-        return \DB::select($query, [$this->threshold]);
+        return $query->get()->toArray();
     }
 
     /**
@@ -131,10 +129,8 @@ class Caregiver1099Query
      */
     public function setFilters(array $filters)
     {
-        foreach ($filters as $filter=>$value){
-            $this->filters[$filter] = [ 'name'=> $filter, 'value' => $value];
+        foreach ($filters as $filter => $value) {
+            $this->filters[$filter] = ['name' => $filter, 'value' => $value];
         }
-
     }
-
 }
--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Expression;
 use Illuminate\Support\ServiceProvider;
 
 class MacroServiceProvider extends ServiceProvider
@@ -14,6 +17,7 @@ class MacroServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->addBcSumToCollection();
+        $this->addJoinSubToQueryBuilder();
     }
 
     /**
@@ -38,5 +42,42 @@ class MacroServiceProvider extends ServiceProvider
                 return add($result, floatval($callback($item)));
             }, floatval(0.00));
         });
+    }
+
+    public function addJoinSubToQueryBuilder() : void
+    {
+        Builder::macro('parseSub', function ($query) {
+            if ($query instanceof self || $query instanceof EloquentBuilder) {
+                return [$query->toSql(), $query->getBindings()];
+            } elseif (is_string($query)) {
+                return [$query, []];
+            } else {
+                throw new InvalidArgumentException;
+            }
+        });
+
+        Builder::macro('createSub', function ($query) {
+            // If the given query is a Closure, we will execute it while passing in a new
+            // query instance to the Closure. This will give the developer a chance to
+            // format and work with the query before we cast it to a raw SQL string.
+            if ($query instanceof Closure) {
+                $callback = $query;
+
+                $callback($query = $this->forSubQuery());
+            }
+
+            return $this->parseSub($query);
+        });
+
+        Builder::macro('joinSub', function ($query, $as, $first, $operator = null, $second = null, $type = 'inner', $where = false) {
+            [$query, $bindings] = $this->createSub($query);
+
+            $expression = '('.$query.') as '.$this->grammar->wrapTable($as);
+
+            $this->addBinding($bindings, 'join');
+
+            return $this->join(new Expression($expression), $first, $operator, $second, $type, $where);
+        });
+
     }
 }
