b4d26bcd8c768c5b90148a7db0a4d999b7a4e74f
faze <jonwillard11@gmail.com>
2019-12-18 14:48:25 -0500

    Create a caregiver yearly earnings table and a command to fill it ALLY-1777

---
+++ app/CaregiverYearlyEarnings.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App;
+
+class CaregiverYearlyEarnings extends BaseModel
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    // **********************************************************
+    // RELATIONSHIPS
+    // **********************************************************
+
+    /**
+     * Get the owning business.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function business()
+    {
+        return $this->belongsTo(Business::class);
+    }
+
+    /**
+     * Get the owning client.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    /**
+     * Get the owning caregiver.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function caregiver()
+    {
+        return $this->belongsTo(Caregiver::class);
+    }
+
+    // **********************************************************
+    // MUTATORS
+    // **********************************************************
+
+    // **********************************************************
+    // QUERY SCOPES
+    // **********************************************************
+
+    // **********************************************************
+    // OTHER FUNCTIONS
+    // **********************************************************
+
+}
