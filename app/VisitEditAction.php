b61fb3f32769fe4574513f1dca32a253ffca8bf0
1337erik <erikpwhite@gmail.com>
2019-12-31 20:27:43 -0500

    created migrations for new tables containing actions and reasons based upon HHA specs

---
+++ app/VisitEditAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VisitEditAction extends Model
+{
+    protected $guarded = ['id'];
+
+}
---
+++ database/migrations/2019_12_30_210912_populate_reason_code_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Billing\VisitEditActionEnum;
+use App\Billing\VisitEditReasonEnum;
+use App\VisitEditAction;
+use App\VisitEditReason;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PopulateReasonCodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach( VisitEditReasonEnum::fullList() as $reason ){
+
+            VisitEditReason::create([
+
+                'code' => $reason[ 'code' ],
+                'description' => $reason[ 'description' ]
+            ]);
+        }
+        foreach( VisitEditActionEnum::fullList() as $action ){
+
+            VisitEditAction::create([
+
+                'code' => $action[ 'code' ],
+                'description' => $action[ 'description' ]
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        VisitEditReason::truncate();
+        VisitEditAction::truncate();
+    }
+}
