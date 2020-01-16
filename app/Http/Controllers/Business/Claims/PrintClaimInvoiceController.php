42a4a727276fd61b9bb9f2bdc78590d6e53fbd9d
1337erik <erikpwhite@gmail.com>
2020-01-16 12:37:37 -0600

    further progress for the form filling

--- app/Http/Controllers/Business/Claims/PrintClaimInvoiceController.php
+++ app/Http/Controllers/Business/Claims/PrintClaimInvoiceController.php
@@ -143,49 +143,26 @@ class PrintClaimInvoiceController extends BaseController
      */
     public function cmsInvoice(ClaimInvoice $claim, Request $request)
     {
+        dd( $claim, $claim->client->primaryPayer, $claim->serviceItems );
         $this->authorize('read', $claim);
+
+        $claim->load([ 'client', 'client.primaryPayer', 'serviceItems', 'business' ]);
+
+        $client = $claim->client;
+        // $address = $client->something;
+        $payer = $client->primaryPayer;
+        $serviceItems = $claim->serviceItems;
+        $business = $claim->business;
 
         $pdf = new Pdf( '../resources/pdf_forms/cms1500/editable_form.pdf' );
 
-        // map the insurance type based on the Client Type
-        switch( 'medicare' ){
+        $todays_date = Carbon::now()->format( 'm/d/Y' );
 
-            case 'medicare':
-
-                $insurance_type = 'medicare';
-                break;
-            case 'medicaide':
-
-                $insurance_type = 'medicaide';
-                break;
-            case 'tricare':
-
-                $insurance_type = 'tricare';
-                break;
-            case 'champva':
-
-                $insurance_type = 'champva';
-                break;
-            case 'group':
-
-                $insurance_type = 'group';
-                break;
-            case 'feca':
-
-                $insurance_type = 'feca';
-                break;
-            default:
-
-                $insurance_type = 'other';
-                break;
-        }
-
-        $business_npi = 123;
+        $client_bday_year = Carbon::parse( $client->date_of_birth )->format( 'Y' );
+        $client_bday_month = Carbon::parse( $client->date_of_birth )->format( 'm' );
+        $client_bday_day = Carbon::parse( $client->date_of_birth )->format( 'd' );
 
         $thing = $pdf->fillForm([
-
-            // check boxes
-            'insurance_type' => $insurance_type, // ACTUALLY DONT CHECK THIS
 
             // text-fields
             'insurance_name'           => 'Go Fuck Yourself Insurance',
@@ -193,15 +170,10 @@ class PrintClaimInvoiceController extends BaseController
             'insurance_address2'       => 'Unit 107',
             'insurance_city_state_zip' => 'Boca raton, Fl, 33431',
 
-            // FIELDS TO ADD
-            // 12 Signature.. SHOULD ALWAYS SAY **SIGNATURE ON FILE**
-            // 13 Signature.. SHOULD ALWAYS SAY **SIGNATURE ON FILE**
-
-
             // Insured Person Information
-            'ins_program_name'        => 'This That Insurance', // $client->payer.. the plan or program name
-            'ins_insurance_id_number' => '12apoplejuice', // $client->hic
-            'ins_name_last_first'     => 'White, Erik', // $client->name_last_first
+            'ins_program_name'        => $payer->payer->name, // $client->payer.. the plan or program name
+            'ins_insurance_id_number' => $client->hic, // $client->hic
+            'ins_name_last_first'     => $client->name_last_first, // $client->name_last_first
             'ins_address'             => '5188 somethign something road', // $client->serviceAddress details
             'ins_city'                => 'fort lauderdale', // $client->serviceAddress details
             'ins_state'               => 'fl', // $client->serviceAddress details
@@ -209,21 +181,16 @@ class PrintClaimInvoiceController extends BaseController
             'ins_phone_area_code'     => '561', // $client->primary_phone_number.. figure this out and break it up
             'ins_phone_number'        => '6999715', // $client->primary_phone_number.. figure this out and break it up
             'ins_policy_number'       => '1337ssdpol13', // $client->payer Policy Number ( HIC for MCO ).. whatever that means
-            'ins_gender_male'         => 'm', // options: [ m, f ] $client->gender
-            'ins_bday_month'          => '05', // $client->bday.. will have to be broken up
-            'ins_bday_day'            => '20', // $client->bday.. will have to be broken up
-            'ins_bday_year'           => '1994', // $client->bday.. will have to be broken up
-
-            'another_benefit_plan' => 'y', // options: [ y, n ] **LEAVE BLANK FOR OFFICE**
-            // below section is only applicable if "another_benefit_plan" above is "y"
-            'ins_2_program_name'        => 'Aetna or someshit', // **LEAVE BLANK FOR OFFICE**
-            'ins_2_name_last_first'     => 'White, Erik', // **LEAVE BLANK FOR OFFICE**
-            'ins_2_insurance_id_number' => '5188 somethign something road', // **LEAVE BLANK FOR OFFICE**
+            'ins_gender'              => strtolower( $client->gender ), // options: [ m, f ] $client->gender
+            'ins_bday_month'          => $client_bday_month, // $client->bday.. will have to be broken up
+            'ins_bday_day'            => $client_bday_day, // $client->bday.. will have to be broken up
+            'ins_bday_year'           => $client_bday_year, // $client->bday.. will have to be broken up
+            'ins_signature'           => 'SIGNATURE ON FILE', // SHOULD ALWAYS SAY **SIGNATURE ON FILE**
 
             'pt_ins_relationship' => 'self', // options: [ self, spouse, child, other ] **ALWAYS DO SELF**
 
             // Patient Information
-            'pt_name_last_first' => 'White, Erik', // $client->name_last_first
+            'pt_name_last_first' => $client->name_last_first, // $client->name_last_first
             'pt_address'         => '5188 somethign something road', // $client->serviceAddress details
             'pt_city'            => 'fort lauderdale', // $client->serviceAddress details
             'pt_state'           => 'fl', // $client->serviceAddress details
@@ -231,23 +198,16 @@ class PrintClaimInvoiceController extends BaseController
             'pt_phone_area_code' => '561', // $client->primary_phone_number.. figure this out and break it up
             'pt_phone_number'    => '6999715', // $client->primary_phone_number.. figure this out and break it up
             'pt_policy_number'   => '1337ssdpol13', // $client->payer Policy Number ( HIC for MCO ).. whatever that means
-            'pt_gender'          => 'm', // options: [ m, f ] $client->gender
-            'pt_bday_month'      => '05', // $client->bday.. will have to be broken up
-            'pt_bday_day'        => '20', // $client->bday.. will have to be broken up
-            'pt_bday_year'       => '1994', // $client->bday.. will have to be broken up
+            'pt_gender'          => strtolower( $client->gender ), // options: [ m, f ] $client->gender
+            'pt_bday_month'      => $client_bday_month, // $client->bday.. will have to be broken up
+            'pt_bday_day'        => $client_bday_day, // $client->bday.. will have to be broken up
+            'pt_bday_year'       => $client_bday_year, // $client->bday.. will have to be broken up
+            'pt_signature'       => 'SIGNATURE ON FILE', // SHOULD ALWAYS SAY **SIGNATURE ON FILE**
+            'pt_todays_date'     => $todays_date,
 
-            'condition_related_to_employment'          => 'n', // options: [ y, n ] **ALWAYS NO**
-            'condition_related_to_auto_accident'       => 'n', // options: [ y, n ] **ALWAYS NO**
-            'condition_related_to_auto_accident_state' => 'FL', // LEAVE BLANK NOT COLLECTED
-            'condition_related_to_other_accident'      => 'n', // options: [ y, n ] **ALWAYS NO**
-
-            'curr_ill_month' => '05', // **LEAVE BLANK FOR OFFICE**
-            'curr_ill_day'   => '20', // **LEAVE BLANK FOR OFFICE**
-            'curr_ill_year'  => '1994', // **LEAVE BLANK FOR OFFICE**
-
-            'ref_physician_name'   => 'Thomas Aquinas', // **LEAVE BLANK FOR OFFICE**
-            'ref_physician_npi'    => 'Thomas Aquinas', // **LEAVE BLANK FOR OFFICE**
-            'ref_physician_number' => 'Thomas Aquinas', // **LEAVE BLANK FOR OFFICE**
+            'condition_related_to_employment'     => 'n', // options: [ y, n ] **ALWAYS NO**
+            'condition_related_to_auto_accident'  => 'n', // options: [ y, n ] **ALWAYS NO**
+            'condition_related_to_other_accident' => 'n', // options: [ y, n ] **ALWAYS NO**
 
             // Check Insurance and Service Auths.. Medical Diagnosis Codes
             'diagnosis_code_1' => '0123',
@@ -263,23 +223,7 @@ class PrintClaimInvoiceController extends BaseController
             'diagnosis_code_11' => '0123',
             'diagnosis_code_12' => '0123',
 
-            'date_unable_to_work_from_month' => '02', // **LEAVE BLANK FOR OFFICE**
-            'date_unable_to_work_from_day'   => '12', // **LEAVE BLANK FOR OFFICE**
-            'date_unable_to_work_from_year'  => '1994', // **LEAVE BLANK FOR OFFICE**
-            'date_unable_to_work_to_month'   => '04', // **LEAVE BLANK FOR OFFICE**
-            'date_unable_to_work_to_day'     => '20', // **LEAVE BLANK FOR OFFICE**
-            'date_unable_to_work_to_year'    => '1994', // **LEAVE BLANK FOR OFFICE**
-
-            'date_hospitalized_from_month' => '02', // **LEAVE BLANK FOR OFFICE**
-            'date_hospitalized_from_day'   => '12', // **LEAVE BLANK FOR OFFICE**
-            'date_hospitalized_from_year'  => '1994', // **LEAVE BLANK FOR OFFICE**
-            'date_hospitalized_to_month'   => '04', // **LEAVE BLANK FOR OFFICE**
-            'date_hospitalized_to_day'     => '20', // **LEAVE BLANK FOR OFFICE**
-            'date_hospitalized_to_year'    => '1994', // **LEAVE BLANK FOR OFFICE**
-
-            'outside_lab' => 'y', // options: [ y, n ] **LEAVE BLANK FOR OFFICE**
-            'outside_lab_charges' => '1337.37', // **LEAVE BLANK FOR OFFICE**
-            'prior_authorization_number' => 'asd123ddsas1zaqwsx',
+            'prior_authorization_number' => 'asd123ddsas1zaqwsx', // TODO => need to figure this one out
 
             'service_1_name'           => 'Testicular Care',
             'service_1_charge_dollars' => 456,
@@ -293,9 +237,9 @@ class PrintClaimInvoiceController extends BaseController
             'service_1_to_year'        => '1994', // $claimable->service->checked_out_time
             'service_1_place'          => 12, // 24.b Place of Service => **ALWAYS 12**
             'service_1_code'           => '', // 24.d CPT ( Service Code ) with MODs => $claim->CPT
-            'service_1_npi'            => $business_npi, // 24.j Pull NPI based off of client branch location
+            'service_1_npi'            => $business->medicaid_npi_number, // 24.j Pull NPI based off of client branch location
 
-            'federal_tax_id'         => $business_npi, // 25 Federal Tax ID => same as 24.j Pull NPI based off of client branch location
+            'federal_tax_id'         => $business->medicaid_npi_number, // 25 Federal Tax ID => same as 24.j Pull NPI based off of client branch location
             'patient_account_number' => '', // 26 Patient Account Number => Pull based off of Ally assigned ID
             'total_charge_dollars'   => 123, // 28
             'total_charge_change'    => 20, // 28 Total Charges => Sum of all charges on page (not claim total)
@@ -303,11 +247,12 @@ class PrintClaimInvoiceController extends BaseController
             'amount_paid_change'     => 20, // 29 Amount Paid => Typically 0 or left blank
             'blanace_due'            => 123.20 - 100.20, // 30 Balance Due => Sum of charges minus payments (on page not claim total)
 
+            // NEED TO CLARIFY
             'supplier_signature'     => '', // 31 Signature of Supplier => Pull from Contacts within Payer Setup 
             'service_provider'       => '', // 32 Service Facility => Location Pull branch specific location info based off branch designation
             'billing_provider'       => '', // 33 Billing Provider => Info Pull branch specific location info based off branch designation
             
-            'todays_date'            => Carbon::now()
+            'todays_date'            => $todays_date
         ])
             ->flatten()
             ->saveAs( '../resources/pdf_forms/cms1500/jeeee.pdf' );
