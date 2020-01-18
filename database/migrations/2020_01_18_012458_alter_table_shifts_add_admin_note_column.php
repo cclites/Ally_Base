58a83e9cf2861d310844e1e938e6e323bded4fd1
1337erik <erikpwhite@gmail.com>
2020-01-17 19:26:31 -0600

    migration to add admin note

---
+++ database/migrations/2020_01_18_012458_alter_table_shifts_add_admin_note_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableShiftsAddAdminNoteColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'claimable_services', function ( Blueprint $table ) {
+
+            $table->string( 'admin_note', 255 )->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table( 'claimable_services', function ( Blueprint $table ) {
+
+            $table->dropColumn( 'admin_note' );
+        });
+    }
+}
