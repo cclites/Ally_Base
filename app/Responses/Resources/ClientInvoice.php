612fb4457e1521796b7e374c4d9fb592f4e69cd1
Jonathan Willard <jonwillard11@gmail.com>
2020-01-24 10:34:46 -0500

    HOTFIX: Remove reference to old claim relationship on client invoices
    Resolves Sentry error ALLY-G0

--- app/Responses/Resources/ClientInvoice.php
+++ app/Responses/Resources/ClientInvoice.php
@@ -68,10 +68,10 @@ class ClientInvoice extends Resource
             'location' => $this->whenLoaded('client', function () {
                 return $this->resource->client->business->name;
             }),
-                'has_claim' => !empty($this->resource->claim),
-                'payment_errors' => $this->whenLoaded('client', function() {
-                    return $this->resource->client->payment_errors;
-                }),
+            'has_claim' => !empty($this->resource->claimInvoices),
+            'payment_errors' => $this->whenLoaded('client', function() {
+                return $this->resource->client->payment_errors;
+            }),
         ];
     }
 }
