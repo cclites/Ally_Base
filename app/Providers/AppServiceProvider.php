655924ac99f7278ef55b186243d6e32fabc82b17
Jonathan Willard <jonwillard11@gmail.com>
2020-01-29 15:24:59 -0500

    Remove service providers related to Xethron/migrations-generator
    No longer needed, package was removed to upgrade to Laravel 6.x

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -152,9 +152,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() === 'local') {
-            $this->app->register(\Way\Generators\GeneratorsServiceProvider::class);
-            $this->app->register(\Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class);
-        }
     }
 }
