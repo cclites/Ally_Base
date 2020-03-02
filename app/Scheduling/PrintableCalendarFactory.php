dae3c97ee03caaba673da45bb19f6988e8399310
Chad Clites <chad@extant.digital>
2020-03-02 12:16:23 -0600

    merging hotfixes

--- app/Scheduling/PrintableCalendarFactory.php
+++ app/Scheduling/PrintableCalendarFactory.php
@@ -82,9 +82,16 @@ class PrintableCalendarFactory
         // Represents the day of the week on which the month starts.
         // $sDay uses calendar_start_week as an offset for registries
         // that do no start their week on a Sunday
-
         $sDay = $startDay->dayOfWeek - ($this->chain->calendar_week_start);
 
+        //If sunday is the start day (day 0) and the registry calendar starts on
+        //Monday, offset. This is a calculated number because a chain can start
+        //their week on any day of the month. Most start their week on a Monday,
+        //just a few start their week on a Sunday.
+        if($sDay < 0){
+            $sDay = 7 - $this->chain->calendar_week_start;
+        }
+        
         $monthName = $startDay->monthName;
 
         // represents an index to know to quit adding calendar days
